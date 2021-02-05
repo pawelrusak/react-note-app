5061d188c44d576102fab0a79cee02dc012c9b52
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-02-05 19:34:17 +0100

    refactor: create <Button />

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,3 +1,11 @@
-const Root = () => <h1>React note app</h1>;
+import Button from 'components/Button/Button';
+
+const Root = () => (
+  <div>
+    <h1>React note app</h1>
+    <Button width="500px">Close / Save</Button>
+    <Button secondary>Remove</Button>
+  </div>
+);
 
 export default Root;
