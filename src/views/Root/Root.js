388b1f46615d9705a8418eed266d57e1084237bd
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-02-05 20:44:50 +0100

    refactor: create global style

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,9 +1,11 @@
 import Button from 'components/Button/Button';
+import GlobalStyle from 'theme/GlobalStyle';
 
 const Root = () => (
   <div>
+    <GlobalStyle />
     <h1>React note app</h1>
-    <Button width="500px">Close / Save</Button>
+    <Button>Close / Save</Button>
     <Button secondary>Remove</Button>
   </div>
 );
