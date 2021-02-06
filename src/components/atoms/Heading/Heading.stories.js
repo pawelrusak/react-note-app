82363e70f11af5eea4c2609853c475761a106aaf
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-02-06 23:50:35 +0100

    chore(my): write the stories of <Heading />

---
+++ src/components/atoms/Heading/Heading.stories.js
@@ -0,0 +1,18 @@
+import Heading from './Heading';
+
+export default {
+  title: 'Heading',
+  component: Heading,
+};
+
+const Template = (args) => <Heading {...args}>Hello, World</Heading>;
+
+export const Normal = Template.bind({});
+Normal.args = {
+  big: false,
+};
+
+export const Big = Template.bind({});
+Big.args = {
+  big: true,
+};
