9fd50df07d0b9719e315419c33fb57905a0006b4
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-02-06 22:09:45 +0100

    chore(my): add control to <Button /> stories

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,12 +1,37 @@
 import Button from './Button';
 
+const colors = {
+  Primary: 'hsl(49, 100%, 58%)',
+  Secondary: 'hsl(196, 83%, 75%)',
+  Tertiary: 'hsl(106, 47%, 64%)',
+};
+
 export default {
   title: 'Button',
   component: Button,
+  argTypes: {
+    color: {
+      control: {
+        type: 'select',
+        options: Object.keys(colors),
+      },
+    },
+    secondary: {
+      table: {
+        disable: true,
+      },
+    },
+  },
 };
 
-// eslint-disable-next-line
-const Template = (args) => <Button {...args}>Hello World</Button>;
+const Template = ({ color, ...args }) => {
+  const selectedColor = colors[color];
+  return (
+    <Button color={selectedColor} {...args}>
+      Hello World
+    </Button>
+  );
+};
 
 export const Primary = Template.bind({});
 Primary.args = {
@@ -16,4 +41,11 @@ Primary.args = {
 export const Secondary = Template.bind({});
 Secondary.args = {
   secondary: true,
+};
+Secondary.argTypes = {
+  color: {
+    table: {
+      disable: true,
+    },
+  },
 };
