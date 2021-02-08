599d236f58708f4f753d8d45c64b7b874292bd86
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-02-08 23:24:13 +0100

    refactor: create basic <Card />

---
+++ src/components/molecules/Card/Card.js
@@ -0,0 +1,65 @@
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
+import Paragraph from 'components/atoms/Paragraph/Paragraph';
+import Heading from 'components/atoms/Heading/Heading';
+import Button from 'components/atoms/Button/Button';
+
+const StyledWrapper = styled.div`
+  min-height: 380px;
+  box-shadow: 0 10px 30px -10px hsla(0, 0%, 0, 0.1);
+  border-radius: 10px;
+  overflow: hidden;
+  position: relative;
+  display: grid;
+  grid-template-rows: 0.25fr 1fr;
+`;
+
+const InnerWrapper = styled.div`
+  position: relative;
+  padding: 17px 30px;
+  background-color: ${({ activeColor, theme }) => (activeColor ? theme[activeColor] : 'white')};
+
+  ${({ flex }) =>
+    flex &&
+    css`
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+    `}
+`;
+
+const DateInfo = styled(Paragraph)`
+  margin: 0 0 5px;
+  font-weight: ${({ theme }) => theme.bold};
+  font-size: ${({ theme }) => theme.fontSize.xs};
+`;
+
+const StyledHeading = styled(Heading)`
+  margin: 5px 0 0;
+`;
+
+const Card = ({ cardType }) => (
+  <StyledWrapper>
+    <InnerWrapper activeColor={cardType}>
+      <StyledHeading>Hello, World!</StyledHeading>
+      <DateInfo>3 days</DateInfo>
+    </InnerWrapper>
+    <InnerWrapper flex>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua.
+      </Paragraph>
+      <Button secondary>REMOVE</Button>
+    </InnerWrapper>
+  </StyledWrapper>
+);
+
+Card.propTypes = {
+  cardType: PropTypes.oneOf(['note', 'twitter', 'article']),
+};
+
+Card.defaultProps = {
+  cardType: 'note',
+};
+
+export default Card;
