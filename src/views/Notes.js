6da9d56cf0cda0678ffd71fcecdf12bed1fae68c
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-02-12 20:02:22 +0100

    refactor: create note views with faker data

---
+++ src/views/Notes.js
@@ -0,0 +1,51 @@
+import UserPageTemplate from 'templates/UserPageTemplate';
+import Card from 'components/molecules/Card/Card';
+
+const notes = [
+  {
+    id: 1,
+    title: 'Wake me up when Vue ends',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '1 day',
+  },
+  {
+    id: 2,
+    title: 'Como es An Gular?',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '1 day',
+  },
+  {
+    id: 3,
+    title: 'Du bist Reactish',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '5 days',
+  },
+  {
+    id: 4,
+    title: 'Reactuj się kto moze!',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '10 days',
+  },
+];
+
+const Notes = () => (
+  <UserPageTemplate pageType="note">
+    <>
+      {notes.map((item) => (
+        <Card
+          cardType="note"
+          title={item.title}
+          content={item.content}
+          created={item.created}
+          key={item.id}
+        />
+      ))}
+    </>
+  </UserPageTemplate>
+);
+
+export default Notes;
