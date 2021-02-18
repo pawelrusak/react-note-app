4bc8b9c2206f77da7421480988e18c41deb9c3cc
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-02-18 21:26:00 +0100

    feat: create articles page of app

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,6 +1,7 @@
 import MainTemplate from 'templates/MainTemplate';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Notes from 'views/Notes';
+import Articles from 'views/Articles';
 
 const Root = () => (
   <BrowserRouter>
@@ -8,6 +9,7 @@ const Root = () => (
       <Switch>
         <Route exact path="/" render={() => <Redirect to="/notes" />} />
         <Route path="/notes" component={Notes} />
+        <Route path="/articles" component={Articles} />
       </Switch>
     </MainTemplate>
   </BrowserRouter>
