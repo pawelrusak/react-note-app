28c29682080d3912e4af6c2ba75c1dd614ad8594
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-02-18 21:56:20 +0100

    feat: create twitters page of app

--- src/views/Root.js
+++ src/views/Root.js
@@ -2,6 +2,7 @@ import MainTemplate from 'templates/MainTemplate';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Notes from 'views/Notes';
 import Articles from 'views/Articles';
+import Twitters from 'views/Twitters';
 
 const Root = () => (
   <BrowserRouter>
@@ -10,6 +11,7 @@ const Root = () => (
         <Route exact path="/" render={() => <Redirect to="/notes" />} />
         <Route path="/notes" component={Notes} />
         <Route path="/articles" component={Articles} />
+        <Route path="/twitters" component={Twitters} />
       </Switch>
     </MainTemplate>
   </BrowserRouter>
