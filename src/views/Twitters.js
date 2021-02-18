c39b970465f9080e14853d05ec647d5763a9e430
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-02-18 21:50:37 +0100

    refactor: create twitters view with faker data

---
+++ src/views/Twitters.js
@@ -0,0 +1,56 @@
+import UserPageTemplate from 'templates/UserPageTemplate';
+import Card from 'components/molecules/Card/Card';
+
+const twitters = [
+  {
+    id: 1,
+    title: 'Hello Roman',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '1 day',
+    twitterName: 'hello_roman',
+  },
+  {
+    id: 2,
+    title: 'Redux guy',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '1 day',
+    twitterName: 'dan_abramov',
+  },
+  {
+    id: 3,
+    title: 'React router stuff',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '5 days',
+    twitterName: 'mjackson',
+  },
+  {
+    id: 4,
+    title: 'Super animacje!',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '10 days',
+    twitterName: 'sarah_edo',
+  },
+];
+
+const Twitters = () => (
+  <UserPageTemplate pageType="twitter">
+    <>
+      {twitters.map((item) => (
+        <Card
+          cardType="twitter"
+          title={item.title}
+          content={item.content}
+          twitterName={item.twitterName}
+          created={item.created}
+          key={item.id}
+        />
+      ))}
+    </>
+  </UserPageTemplate>
+);
+
+export default Twitters;
