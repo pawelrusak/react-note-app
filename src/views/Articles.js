a28aa471edbad2f257a5c5229410f32be9808e8e
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-02-18 21:14:39 +0100

    refactor: create articles view with faker data

---
+++ src/views/Articles.js
@@ -0,0 +1,56 @@
+import UserPageTemplate from 'templates/UserPageTemplate';
+import Card from 'components/molecules/Card/Card';
+
+const articles = [
+  {
+    id: 1,
+    title: 'React on my mind',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    articleUrl: 'https://youtube.com/helloroman',
+    created: '1 day',
+  },
+  {
+    id: 2,
+    title: 'Wish you React',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    articleUrl: 'https://youtube.com/helloroman',
+    created: '1 day',
+  },
+  {
+    id: 3,
+    title: 'You gave React a bad name',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    articleUrl: 'https://youtube.com/helloroman',
+    created: '5 days',
+  },
+  {
+    id: 4,
+    title: 'Is it React you looking for?',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    articleUrl: 'https://youtube.com/helloroman',
+    created: '10 days',
+  },
+];
+
+const Articles = () => (
+  <UserPageTemplate pageType="article">
+    <>
+      {articles.map((item) => (
+        <Card
+          cardType="article"
+          title={item.title}
+          content={item.content}
+          articleUrl={item.articleUrl}
+          created={item.created}
+          key={item.id}
+        />
+      ))}
+    </>
+  </UserPageTemplate>
+);
+
+export default Articles;
