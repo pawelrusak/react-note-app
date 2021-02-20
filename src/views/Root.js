ac733a792beebf1f9dddb6de7d384544c210852e
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-02-20 22:46:13 +0100

    refactor: connect Redux to React

--- src/views/Root.js
+++ src/views/Root.js
@@ -4,22 +4,26 @@ import Notes from 'views/Notes';
 import Articles from 'views/Articles';
 import Twitters from 'views/Twitters';
 import { routes } from 'routes';
+import { Provider } from 'react-redux';
+import store from 'store';
 import DetailsPage from './DetailsPage';
 
 const Root = () => (
-  <BrowserRouter>
-    <MainTemplate>
-      <Switch>
-        <Route exact path={routes.home} render={() => <Redirect to="/notes" />} />
-        <Route exact path={routes.notes} component={Notes} />
-        <Route path={routes.note} component={DetailsPage} />
-        <Route exact path={routes.articles} component={Articles} />
-        <Route path={routes.article} component={DetailsPage} />
-        <Route exact path={routes.twitters} component={Twitters} />
-        <Route path={routes.twitter} component={DetailsPage} />
-      </Switch>
-    </MainTemplate>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <MainTemplate>
+        <Switch>
+          <Route exact path={routes.home} render={() => <Redirect to="/notes" />} />
+          <Route exact path={routes.notes} component={Notes} />
+          <Route path={routes.note} component={DetailsPage} />
+          <Route exact path={routes.articles} component={Articles} />
+          <Route path={routes.article} component={DetailsPage} />
+          <Route exact path={routes.twitters} component={Twitters} />
+          <Route path={routes.twitter} component={DetailsPage} />
+        </Switch>
+      </MainTemplate>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default Root;
