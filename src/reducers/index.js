1504ebc9d83efb765d740770f82b9fd775d5edab
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-02-20 22:52:27 +0100

    refactor: add more dummy dataset to Redux state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,39 +1,107 @@
+import { v4 as getUuid } from 'uuid';
+
 const initialState = {
   twitters: [
     {
-      id: 1,
+      id: getUuid(),
       title: 'Hello Roman',
       content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
       created: '1 day',
       twitterName: 'hello_roman',
     },
-  ],
-  articles: [
     {
-      id: 1,
-      title: 'Hello Roman',
+      id: getUuid(),
+      title: 'Redux guy',
       content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
       created: '1 day',
+      twitterName: 'dan_abramov',
+    },
+    {
+      id: getUuid(),
+      title: 'React router stuff',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '5 days',
+      twitterName: 'mjackson',
+    },
+    {
+      id: getUuid(),
+      title: 'Super animacje!',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '10 days',
+      twitterName: 'sarah_edo',
+    },
+  ],
+  articles: [
+    {
+      id: getUuid(),
+      title: 'React on my mind',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
       articleUrl: 'https://youtube.com/helloroman',
+      created: '1 day',
+    },
+    {
+      id: getUuid(),
+      title: 'Wish you React',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      articleUrl: 'https://youtube.com/helloroman',
+      created: '1 day',
+    },
+    {
+      id: getUuid(),
+      title: 'You gave React a bad name',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      articleUrl: 'https://youtube.com/helloroman',
+      created: '5 days',
+    },
+    {
+      id: getUuid(),
+      title: 'Is it React you looking for?',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      articleUrl: 'https://youtube.com/helloroman',
+      created: '10 days',
     },
   ],
   notes: [
     {
-      id: 1,
-      title: 'Hello Roman',
+      id: getUuid(),
+      title: 'Wake me up when Vue ends',
       content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
       created: '1 day',
+    },
+    {
+      id: getUuid(),
+      title: 'Como es An Gular?',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '1 day',
+    },
+    {
+      id: getUuid(),
+      title: 'Du bist Reactish',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '5 days',
+    },
+    {
+      id: getUuid(),
+      title: 'Reactuj się kto moze!',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '10 days',
     },
   ],
 };
 
 // eslint-disable-next-line no-unused-vars
-const rootReducer = (state = initialState, action) => {
-  // eslint-disable-next-line no-console
-  console.log(action);
-};
+const rootReducer = (state = initialState, action) => state;
 
 export default rootReducer;
