0b8371b5c7772cc20d1d78bf479ea80a5489359a
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-02-21 18:04:54 +0100

    refactor: create withContext HOC

---
+++ src/hoc/withContext.js
@@ -0,0 +1,13 @@
+import PageContext from 'context';
+
+const withContext = (Component) =>
+  function contextComponent(props) {
+    return (
+      <PageContext.Consumer>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {(context) => <Component {...props} pageContext={context} />}
+      </PageContext.Consumer>
+    );
+  };
+
+export default withContext;
