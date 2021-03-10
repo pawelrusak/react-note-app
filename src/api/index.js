d0df45927b21a1951c40bc0629e700ad1a608fa2
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-03-11 00:04:09 +0100

    refactor(my): create functions for fetch items

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,17 @@
+import { convertQuerySnapshot } from 'utils';
+import { queryNotesByTypeAndUserID } from './queries';
 import { auth } from './firebase';
 
 export const authenticateUser = (email, password) =>
   auth.signInWithEmailAndPassword(email, password);
+
+export const fetchItems = async ({ type, userID } = {}) => {
+  try {
+    const querySnapshotResponse = await queryNotesByTypeAndUserID(type, userID);
+    const data = convertQuerySnapshot(querySnapshotResponse);
+
+    return Promise.resolve({ data });
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
