96f1dd293403c332f43a2e0c350272022431bd71
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-03-10 23:18:27 +0100

    refactor(my): setup the database service

--- src/api/firebase/index.js
+++ src/api/firebase/index.js
@@ -1,7 +1,9 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
 import config from './config';
 
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
+export const db = firebase.firestore();
