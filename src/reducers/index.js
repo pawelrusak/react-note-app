2704e541b429bb6d8e23fb72bdef4480f2f25261
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-03-11 18:17:21 +0100

    refactor: add fetch success action for reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,109 +1,23 @@
-import { v4 as getUuid } from 'uuid';
-import { ADD_ITEM, REMOVE_ITEM, AUTH_SUCCESS } from 'actions';
+import {
+  ADD_ITEM,
+  REMOVE_ITEM,
+  AUTH_SUCCESS,
+  // eslint-disable-next-line
+  FETCH_REQUEST,
+  FETCH_SUCCESS,
+  // eslint-disable-next-line
+  FETCH_FAILURE,
+} from 'actions';
 
-const initialState = {
-  twitters: [
-    {
-      id: getUuid(),
-      title: 'Hello Roman',
-      content:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-      created: '1 day',
-      twitterName: 'hello_roman',
-    },
-    {
-      id: getUuid(),
-      title: 'Redux guy',
-      content:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-      created: '1 day',
-      twitterName: 'dan_abramov',
-    },
-    {
-      id: getUuid(),
-      title: 'React router stuff',
-      content:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-      created: '5 days',
-      twitterName: 'mjackson',
-    },
-    {
-      id: getUuid(),
-      title: 'Super animacje!',
-      content:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-      created: '10 days',
-      twitterName: 'sarah_edo',
-    },
-  ],
-  articles: [
-    {
-      id: getUuid(),
-      title: 'React on my mind',
-      content:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-      articleUrl: 'https://youtube.com/helloroman',
-      created: '1 day',
-    },
-    {
-      id: getUuid(),
-      title: 'Wish you React',
-      content:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-      articleUrl: 'https://youtube.com/helloroman',
-      created: '1 day',
-    },
-    {
-      id: getUuid(),
-      title: 'You gave React a bad name',
-      content:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-      articleUrl: 'https://youtube.com/helloroman',
-      created: '5 days',
-    },
-    {
-      id: getUuid(),
-      title: 'Is it React you looking for?',
-      content:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-      articleUrl: 'https://youtube.com/helloroman',
-      created: '10 days',
-    },
-  ],
-  notes: [
-    {
-      id: getUuid(),
-      title: 'Wake me up when Vue ends',
-      content:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-      created: '1 day',
-    },
-    {
-      id: getUuid(),
-      title: 'Como es An Gular?',
-      content:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-      created: '1 day',
-    },
-    {
-      id: getUuid(),
-      title: 'Du bist Reactish',
-      content:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-      created: '5 days',
-    },
-    {
-      id: getUuid(),
-      title: 'Reactuj się kto moze!',
-      content:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-      created: '10 days',
-    },
-  ],
-};
+const initialState = {};
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        [action.payload.itemType]: [...action.payload.data],
+      };
     /**
      * You can find the original code of the course in the link below.
      *
