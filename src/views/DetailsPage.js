9cacde75c4b7ca024fb9f8eb966b1c8f3aaf89a7
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-03-12 12:58:03 +0100

    feat: fetch items in the twitters page

--- src/views/DetailsPage.js
+++ src/views/DetailsPage.js
@@ -1,23 +1,66 @@
+import { Component } from 'react';
 import DetailsTemplate from 'templates/DetailsTemplate';
+import withContext from 'hoc/withContext';
+import { connect } from 'react-redux';
+import { fetchItem } from 'api';
 
-const dummyArticle = {
-  id: 1,
-  title: 'Wake me up when Vue ends',
-  content:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-  twitterName: 'hello_roman',
-  articleUrl: 'https://youtube.com/helloroman',
-  created: '1 day',
+class DetailsPage extends Component {
+  state = {
+    activeItem: {
+      title: '',
+      content: '',
+      articleUrl: '',
+      twitterName: '',
+    },
+  };
+
+  componentDidMount() {
+    // eslint-disable-next-line react/prop-types
+    const { activeItem } = this.props;
+    if (activeItem) {
+      const [item] = activeItem;
+      this.setState({ activeItem: item });
+    } else {
+      const {
+        // eslint-disable-next-line react/prop-types
+        match: {
+          // eslint-disable-next-line react/prop-types
+          params: { id },
+        },
+      } = this.props;
+      fetchItem(id)
+        .then(({ data }) => {
+          this.setState({ activeItem: data });
+        })
+        // eslint-disable-next-line no-console
+        .catch((err) => console.log(err));
+    }
+  }
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <DetailsTemplate
+        title={activeItem.title}
+        created={activeItem.created}
+        content={activeItem.content}
+        articleUrl={activeItem.articleUrl}
+        twitterName={activeItem.twitterName}
+      />
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  if (state[ownProps.pageContext]) {
+    return {
+      activeItem: state[ownProps.pageContext].filter(
+        (item) => item.id === ownProps.match.params.id,
+      ),
+    };
+  }
+  return {};
 };
 
-const DetailsPage = () => (
-  <DetailsTemplate
-    title={dummyArticle.title}
-    created={dummyArticle.created}
-    content={dummyArticle.content}
-    articleUrl={dummyArticle.articleUrl}
-    twitterName={dummyArticle.twitterName}
-  />
-);
-
-export default DetailsPage;
+export default withContext(connect(mapStateToProps)(DetailsPage));
