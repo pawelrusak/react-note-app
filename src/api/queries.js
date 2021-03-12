106bbac0bce2c98bd1d50e6dc13b21963b8228b8
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-03-12 13:45:50 +0100

    refactor(my): create reference for firebase data

--- src/api/queries.js
+++ src/api/queries.js
@@ -1,6 +1,8 @@
-import { db } from './firebase';
+import { notesCollectionRef, getNoteDocumentRefById } from './refs';
 
 export const queryItemsByTypeAndUserID = (type, userID = null) =>
-  db.collection('notes').where('userID', '==', userID).where('type', '==', type).get();
+  notesCollectionRef.where('userID', '==', userID).where('type', '==', type).get();
 
-export const queryItemByID = (id) => db.collection('notes').doc(id).get();
+export const queryItemByID = (id) => getNoteDocumentRefById(id).get();
+
+export const queryRemoveItemByID = (id) => getNoteDocumentRefById(id).delete();
