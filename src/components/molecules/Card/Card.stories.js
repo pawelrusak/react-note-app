8ac4b87d31a42f57e0de2538b1227b639f1d00b8
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-03-25 18:43:05 +0100

    chore: create the story for the default <Card />

--- src/components/molecules/Card/Card.stories.js
+++ src/components/molecules/Card/Card.stories.js
@@ -9,13 +9,18 @@ export default {
 
 const Template = (args) => <Card {...args} />;
 
-export const Note = Template.bind({});
-Note.args = {
+export const Default = Template.bind({});
+Default.args = {
   id: '8885d2d6-b081-4342-8232-e889affa9d93',
   title: 'My best note ever',
   created: '3 days',
   content:
     'Miles Dewey Davis III (May 26, 1926 - September 28, 1991) was an American jazz trumpeter, bandleader, and composer.',
+};
+
+export const Note = Template.bind({});
+Note.args = {
+  ...Default.args,
 };
 Note.parameters = {
   pageContext: 'notes',
@@ -23,7 +28,7 @@ Note.parameters = {
 
 export const Twitter = Template.bind({});
 Twitter.args = {
-  ...Note.args,
+  ...Default.args,
   twitterName: 'hello_romans',
 };
 Twitter.parameters = {
@@ -32,7 +37,7 @@ Twitter.parameters = {
 
 export const Article = Template.bind({});
 Article.args = {
-  ...Note.args,
+  ...Default.args,
   articleUrl: 'https://youtube.com/helloroman',
 };
 Article.parameters = {
