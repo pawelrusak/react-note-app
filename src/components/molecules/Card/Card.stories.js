f743a5dbee716114988510676106ce498cb896c0
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-03-25 17:50:26 +0100

    chore: refactor the stories of the <Card />
    refactor arguments in the stories of the <Card />

--- src/components/molecules/Card/Card.stories.js
+++ src/components/molecules/Card/Card.stories.js
@@ -10,14 +10,6 @@ const CardWrapper = styled.div`
 export default {
   title: 'Molecules/Card',
   component: Card,
-  args: {
-    id: '8885d2d6-b081-4342-8232-e889affa9d93',
-    title: 'My best note ever',
-    created: '3 days',
-    content: `Miles Dewey Davis III (May 26, 1926 - 
-       September 28, 1991) was an American jazz
-       trumpeter, bandleader, and composer.`,
-  },
   decorators: [
     (Story) => (
       <Provider store={store}>
@@ -35,13 +27,20 @@ export default {
 const Template = (args) => <Card {...args} />;
 
 export const Note = Template.bind({});
-Note.args = {};
+Note.args = {
+  id: '8885d2d6-b081-4342-8232-e889affa9d93',
+  title: 'My best note ever',
+  created: '3 days',
+  content:
+    'Miles Dewey Davis III (May 26, 1926 - September 28, 1991) was an American jazz trumpeter, bandleader, and composer.',
+};
 Note.parameters = {
   pageContext: 'notes',
 };
 
 export const Twitter = Template.bind({});
 Twitter.args = {
+  ...Note.args,
   twitterName: 'hello_romans',
 };
 Twitter.parameters = {
@@ -50,6 +49,7 @@ Twitter.parameters = {
 
 export const Article = Template.bind({});
 Article.args = {
+  ...Note.args,
   articleUrl: 'https://youtube.com/helloroman',
 };
 Article.parameters = {
