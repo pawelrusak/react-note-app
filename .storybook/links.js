4ff9ad76e09a1e1d8751098bfaaf82d5bc7686c8
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-03-27 14:31:13 +0100

    chore: add links to the stories of <Sidebar />

---
+++ .storybook/links.js
@@ -0,0 +1,7 @@
+import { linkTo } from '@storybook/addon-links';
+
+export const sidebarLinks = {
+  '/notes': linkTo('Organisms/Sidebar', 'Note'),
+  '/articles': linkTo('Organisms/Sidebar', 'Article'),
+  '/twitters': linkTo('Organisms/Sidebar', 'Twitter'),
+};
--- src/components/organisms/Sidebar/Sidebar.stories.js
+++ src/components/organisms/Sidebar/Sidebar.stories.js
@@ -1,27 +1,31 @@
 import StoryRouter from 'storybook-react-router';
+import { sidebarLinks } from '../../../../.storybook/links';
 import Sidebar from './Sidebar';
 
 export default {
   title: 'Organisms/Sidebar',
   component: Sidebar,
-  decorators: [StoryRouter()],
 };
 
 const Template = (args) => <Sidebar {...args} />;
 
 export const Default = Template.bind({});
+Default.decorators = [StoryRouter(sidebarLinks)];
 
 export const Note = Template.bind({});
+Note.decorators = [StoryRouter(sidebarLinks, { initialEntries: ['/notes'] })];
 Note.parameters = {
   pageContext: 'notes',
 };
 
 export const Twitter = Template.bind({});
+Twitter.decorators = [StoryRouter(sidebarLinks, { initialEntries: ['/twitters'] })];
 Twitter.parameters = {
   pageContext: 'twitters',
 };
 
 export const Article = Template.bind({});
+Article.decorators = [StoryRouter(sidebarLinks, { initialEntries: ['/articles'] })];
 Article.parameters = {
   pageContext: 'articles',
 };
