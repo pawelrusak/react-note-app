77423f588773724ff0d6e1aac9f9e5157c01d412
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-03-27 18:47:30 +0100

    chore: disable SB warning in the <Sidebar />
    disable StoryBook warning in the stories of the <Sidebar />

--- src/components/organisms/Sidebar/Sidebar.stories.js
+++ src/components/organisms/Sidebar/Sidebar.stories.js
@@ -11,21 +11,27 @@ const Template = (args) => <Sidebar {...args} />;
 
 export const Default = Template.bind({});
 Default.decorators = [StoryRouter(sidebarLinks)];
+Default.parameters = {
+  controls: { hideNoControlsWarning: true },
+};
 
 export const Note = Template.bind({});
 Note.decorators = [StoryRouter(sidebarLinks, { initialEntries: ['/notes'] })];
 Note.parameters = {
+  ...Default.parameters,
   pageContext: 'notes',
 };
 
 export const Twitter = Template.bind({});
 Twitter.decorators = [StoryRouter(sidebarLinks, { initialEntries: ['/twitters'] })];
 Twitter.parameters = {
+  ...Default.parameters,
   pageContext: 'twitters',
 };
 
 export const Article = Template.bind({});
 Article.decorators = [StoryRouter(sidebarLinks, { initialEntries: ['/articles'] })];
 Article.parameters = {
+  ...Default.parameters,
   pageContext: 'articles',
 };
