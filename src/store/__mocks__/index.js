d51de60cba50f6183056c1b552d50b6d9471d77c
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-03-28 21:25:47 +0200

    chore: add sample dataset to mocked store

--- src/store/__mocks__/index.js
+++ src/store/__mocks__/index.js
@@ -2,7 +2,18 @@ import configureStore from 'redux-mock-store';
 
 const middlewares = [];
 const mockStore = configureStore(middlewares);
-const initialState = {};
+const initialState = {
+  articles: [
+    {
+      id: '8885d2d6-b081-4342-8232-e889affa9d93',
+      title: 'My best note ever',
+      created: '3 days',
+      content:
+        'Miles Dewey Davis III (May 26, 1926 - September 28, 1991) was an American jazz trumpeter, bandleader, and composer.',
+      articleUrl: 'https://youtube.com/helloroman',
+    },
+  ],
+};
 const store = mockStore(initialState);
 
 export default store;
