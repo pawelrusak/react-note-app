1dac6c4fad7bf9c42f9424fc4cce22f84caefba4
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-03-28 21:55:57 +0200

    chore: add thunk middleware to mocked store

--- src/store/__mocks__/index.js
+++ src/store/__mocks__/index.js
@@ -1,4 +1,5 @@
 import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 
 const sampleNote = {
   id: '8885d2d6-b081-4342-8232-e889affa9d93',
@@ -8,7 +9,7 @@ const sampleNote = {
     'Miles Dewey Davis III (May 26, 1926 - September 28, 1991) was an American jazz trumpeter, bandleader, and composer.',
 };
 
-const middlewares = [];
+const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 const initialState = {
   notes: [
