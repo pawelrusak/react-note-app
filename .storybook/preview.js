12a9ba477686b537bc04856387e341679f7d3acd
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-03-29 15:21:11 +0200

    chore: set off control warning in global config
    turn off control warning in the Storybook global configuration

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import { PageContextDecorator, ThemeDecorator } from './decorators';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: { hideNoControlsWarning: true },
 };
 
 export const decorators = [ThemeDecorator, PageContextDecorator];
