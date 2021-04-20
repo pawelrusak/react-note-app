16dc0ea1d2e1124da12efa49b4ada161923c9b03
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-04-20 17:14:38 +0200

    test: create render function with providers
    create custom render function with providers for tests

---
+++ src/testUtils.js
@@ -0,0 +1,37 @@
+import { render as rtlRender } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from 'store';
+import PropTypes from 'prop-types';
+import PageContext from 'context';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'theme/mainTheme';
+
+const render = (ui, { path = '/', pageType = 'notes', ...renderOptions } = {}) => {
+  window.history.pushState({}, 'Test page', path);
+
+  const Wrapper = ({ children }) => (
+    <BrowserRouter>
+      <Provider store={store}>
+        <PageContext.Provider value={pageType}>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </PageContext.Provider>
+      </Provider>
+    </BrowserRouter>
+  );
+
+  Wrapper.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+
+  return {
+    ...rtlRender(ui, {
+      wrapper: Wrapper,
+      ...renderOptions,
+    }),
+    store,
+  };
+};
+
+export * from '@testing-library/react';
+export { render, rtlRender };
