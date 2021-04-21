70b0a98ae9d40563d221eef082c8d0ecca157ea3
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-04-21 20:51:30 +0200

    test: refactor test component builder

--- src/testComponent.js
+++ src/testComponent.js
@@ -2,13 +2,13 @@ class TestComponentBuilder {
   // [toHaveFlag, name, element]
   #tests = [];
 
-  #testComponentName = null;
+  #componentName = null;
 
-  #testComponentRender = null;
+  #componentRender = null;
 
   constructor(name, render) {
-    this.#testComponentName = name;
-    this.#testComponentRender = render;
+    this.#componentName = name;
+    this.#componentRender = render;
   }
 
   toBeInTheDocument = (name, element) => {
@@ -23,15 +23,11 @@ class TestComponentBuilder {
 
   start = () => {
     this.#tests.forEach(([testToHave, name, element]) => {
-      test(`${this.#testComponentName} ${testToHave ? 'have' : 'not have'} ${name}`, () => {
-        this.#testComponentRender();
-        const handleExpect = expect(element());
+      test(`${this.#componentName} ${testToHave ? 'have' : 'not have'} ${name}`, () => {
+        this.#componentRender();
+        const expectElement = expect(element());
 
-        if (testToHave) {
-          return handleExpect.toBeInTheDocument();
-        }
-
-        return handleExpect.not.toBeInTheDocument();
+        (testToHave ? expectElement : expectElement.not).toBeInTheDocument();
       });
     });
   };
