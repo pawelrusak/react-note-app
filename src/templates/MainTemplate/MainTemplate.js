0d5e76d4f11e05cb0c855ea91182ffb4e0cbe62c
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-05-10 12:16:42 +0200

    refactor: use custom hook in <MainTemplate />

--- src/templates/MainTemplate/MainTemplate.js
+++ src/templates/MainTemplate/MainTemplate.js
@@ -1,27 +1,16 @@
-import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'theme/GlobalStyle';
 import PageContext from 'context';
 import { theme } from 'theme/mainTheme';
-import { useLocation } from 'react-router-dom';
+import { useCurrentPageType } from 'hooks';
 
 const MainTemplate = ({ children }) => {
-  const [pageType, setPageType] = useState('notes');
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    // set current page type
-    const pageTypes = ['twitters', 'articles', 'notes'];
-
-    const [currentPage] = pageTypes.filter((page) => pathname.includes(page));
-
-    setPageType(currentPage);
-  }, [pathname]);
+  const currentPageType = useCurrentPageType();
 
   return (
     <div>
-      <PageContext.Provider value={pageType}>
+      <PageContext.Provider value={currentPageType}>
         <GlobalStyle />
         <ThemeProvider theme={theme}>{children}</ThemeProvider>
       </PageContext.Provider>
