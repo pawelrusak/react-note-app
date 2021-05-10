60bddc22a60a1e6f133c5b797af21f00bc109e80
Paweł Rusak <78496242+pawelrusak@users.noreply.github.com>
2021-05-10 20:40:06 +0200

    refactor: modernize the <Twitters />  (#11)
    * refactor: improve prop types in the <Twitters />
    
    * refactor: convert <Twitters /> to fn component
    
    * refactor: use Redux hooks in <Twitters />
    
    * refactor: create hook to fetch items by type
    
    * refactor: use custom hook in <Twitters />

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -3,3 +3,4 @@ export { usePageTypeContext } from './custom/usePageTypeContext';
 export { useHistoryPush } from './custom/useHistoryPush';
 export { useRemoveItemAction } from './custom/useRemoveItemAction';
 export { useCurrentPageType } from './custom/useCurrentPageType';
+export { useFetchItems } from './custom/useFetchItems';
--- src/views/Twitters/Twitters.js
+++ src/views/Twitters/Twitters.js
@@ -1,61 +1,25 @@
-import { Component } from 'react';
 import GridTemplate from 'templates/GridTemplate/GridTemplate';
 import Card from 'components/molecules/Card/Card';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { fetchItems } from 'actions';
+import { useFetchItems } from 'hooks';
 
-class Twitters extends Component {
-  componentDidMount() {
-    // eslint-disable-next-line
-    const { fetchTwitters } = this.props;
-    fetchTwitters();
-  }
+const Twitters = () => {
+  const twitters = useFetchItems('twitters');
 
-  render() {
-    const { twitters } = this.props;
-
-    return (
-      <GridTemplate>
-        {twitters.map(({ id, title, content, twitterName, created }) => (
-          <Card
-            id={id}
-            cardType="twitters"
-            title={title}
-            content={content}
-            twitterName={twitterName}
-            created={created}
-            key={id}
-          />
-        ))}
-      </GridTemplate>
-    );
-  }
-}
-
-Twitters.propTypes = {
-  twitters: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      title: PropTypes.string.isRequired,
-      content: PropTypes.string.isRequired,
-      twitterName: PropTypes.string.isRequired,
-      created: PropTypes.string.isRequired,
-    }),
-  ),
+  return (
+    <GridTemplate>
+      {twitters.map(({ id, title, content, twitterName, created }) => (
+        <Card
+          id={id}
+          cardType="twitters"
+          title={title}
+          content={content}
+          twitterName={twitterName}
+          created={created}
+          key={id}
+        />
+      ))}
+    </GridTemplate>
+  );
 };
 
-Twitters.defaultProps = {
-  twitters: [],
-};
-
-const mapStateToProps = (state) => {
-  const { twitters } = state;
-  return { twitters };
-};
-
-const mapDispatchToProps = (dispatch) => ({
-  fetchTwitters: () => dispatch(fetchItems('twitters')),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Twitters);
+export default Twitters;
