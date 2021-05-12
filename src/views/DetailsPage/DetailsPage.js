a56acb3a64048a417d929c1b13a4c37d4635f8f0
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-05-13 01:02:16 +0200

    refactor: use custom hook in <DetailsPage />

--- src/views/DetailsPage/DetailsPage.js
+++ src/views/DetailsPage/DetailsPage.js
@@ -1,37 +1,8 @@
-import { useEffect, useState } from 'react';
 import DetailsTemplate from 'templates/DetailsTemplate/DetailsTemplate';
-import { useSelector } from 'react-redux';
-import { fetchItem } from 'api';
-import { useParams } from 'react-router-dom';
-import { usePageTypeContext } from 'hooks';
+import { useActiveItem } from 'hooks';
 
 const DetailsPage = () => {
-  const pageType = usePageTypeContext();
-  const { id: itemID } = useParams();
-  const storeActiveItem = useSelector((store) =>
-    store?.[pageType]?.find((item) => item.id === itemID),
-  );
-  const [activeItem, setActiveItem] = useState({
-    title: '',
-    content: '',
-    created: '',
-    articleUrl: '',
-    twitterName: '',
-  });
-
-  useEffect(() => {
-    if (storeActiveItem) {
-      setActiveItem({ ...storeActiveItem });
-    } else {
-      fetchItem(itemID)
-        .then(({ data: item }) => {
-          setActiveItem({ ...item });
-        })
-        // eslint-disable-next-line no-console
-        .catch((err) => console.log(err));
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const activeItem = useActiveItem();
 
   return (
     <DetailsTemplate
