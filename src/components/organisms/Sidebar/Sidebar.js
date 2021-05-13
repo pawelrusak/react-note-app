67e43e8ba65e7ad7c0be34bcd381d4139c480189
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-05-13 15:56:15 +0200

    refactor: replace HOC by hook in <Sidebar />
    replace withContext HOC by hook in <Sidebar />

--- src/components/organisms/Sidebar/Sidebar.js
+++ src/components/organisms/Sidebar/Sidebar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import ButtonIcon from 'components/atoms/ButtonIcon/ButtonIcon';
 import bulbIcon from 'assets/icons/bulb.svg';
@@ -8,7 +7,7 @@ import logoutIcon from 'assets/icons/logout.svg';
 import penIcon from 'assets/icons/pen.svg';
 import twitterIcon from 'assets/icons/twitter.svg';
 import logoIcon from 'assets/icons/logo.svg';
-import withContext from 'hoc/withContext';
+import { usePageTypeContext } from 'hooks';
 
 const StyledWrapper = styled.nav`
   position: fixed;
@@ -46,30 +45,26 @@ const StyledLinksList = styled.ul`
   list-style: none;
 `;
 
-const Sidebar = ({ pageContext }) => (
-  <StyledWrapper activeColor={pageContext}>
-    <StyledLogoLink to="/" />
-    <StyledLinksList>
-      <li>
-        <ButtonIcon as={NavLink} to="/notes" icon={penIcon} activeclass="active" />
-      </li>
-      <li>
-        <ButtonIcon as={NavLink} to="/twitters" icon={twitterIcon} activeclass="active" />
-      </li>
-      <li>
-        <ButtonIcon as={NavLink} to="/articles" icon={bulbIcon} activeclass="active" />
-      </li>
-    </StyledLinksList>
-    <StyledLogoutButton as={NavLink} to="/login" icon={logoutIcon} />
-  </StyledWrapper>
-);
+const Sidebar = () => {
+  const pageContext = usePageTypeContext();
 
-Sidebar.propTypes = {
-  pageContext: PropTypes.oneOf(['notes', 'twitters', 'articles']),
+  return (
+    <StyledWrapper activeColor={pageContext}>
+      <StyledLogoLink to="/" />
+      <StyledLinksList>
+        <li>
+          <ButtonIcon as={NavLink} to="/notes" icon={penIcon} activeclass="active" />
+        </li>
+        <li>
+          <ButtonIcon as={NavLink} to="/twitters" icon={twitterIcon} activeclass="active" />
+        </li>
+        <li>
+          <ButtonIcon as={NavLink} to="/articles" icon={bulbIcon} activeclass="active" />
+        </li>
+      </StyledLinksList>
+      <StyledLogoutButton as={NavLink} to="/login" icon={logoutIcon} />
+    </StyledWrapper>
+  );
 };
 
-Sidebar.defaultProps = {
-  pageContext: 'notes',
-};
-
-export default withContext(Sidebar);
+export default Sidebar;
