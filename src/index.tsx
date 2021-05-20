6a21749cd50455fac2d20c6cd4caf2d8add19e29
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-05-20 21:49:04 +0200

    chore: setup TypeScript

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/jsx-filename-extension, no-use-before-define */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from 'views/Root';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
