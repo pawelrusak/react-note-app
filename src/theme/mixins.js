c39b3a33b42e5e6df906cbf8b9ab91854cda4bf5
Paweł Rusak <78496242+pawelrusak@users.noreply.github.com>
2021-05-26 08:35:40 +0200

    refactor: convert activecolor mixin to TS (#24)
    * refactor: convert styled activecolor mixin to TS
    
    * refactor: create type for activecolor mixin

--- src/theme/mixins.js
+++
@@ -1,6 +0,0 @@
-import { css } from 'styled-components';
-
-export const activecolor = css`
-  background-color: ${(props) =>
-    props.activecolor ? props.theme[props.activecolor] : props.theme.notes};
-`;
---
+++ src/theme/mixins.ts
@@ -0,0 +1,9 @@
+import { css } from 'styled-components';
+import { ItemVariants } from 'commonTypes';
+
+export type ActiveColorArgs = { readonly activecolor?: ItemVariants };
+
+export const activecolor = css<ActiveColorArgs>`
+  background-color: ${(props) =>
+    props.activecolor ? props.theme[props.activecolor] : props.theme.notes};
+`;
