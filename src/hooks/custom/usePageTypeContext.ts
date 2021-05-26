351853f38af509533d50cad9dcb6e81bc0b76ea4
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-05-26 06:46:14 +0200

    refactor: convert the usePageTypeContext to TS

--- src/hooks/custom/usePageTypeContext.ts
+++ src/hooks/custom/usePageTypeContext.ts
@@ -1,7 +1,8 @@
 import { useContext } from 'react';
 import PageContext from 'context';
+import { ItemVariants } from 'commonTypes';
 
-export const usePageTypeContext = (initialValue = 'notes') => {
+export const usePageTypeContext = (initialValue: ItemVariants = 'notes'): ItemVariants => {
   const pageType = useContext(PageContext);
 
   return pageType ?? initialValue;
