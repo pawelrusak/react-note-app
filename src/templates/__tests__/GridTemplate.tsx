4e803761e450c33267f4fa80982029e936a59157
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-05-26 14:24:32 +0200

    test: convert test of <Card /> to TypeScript

--- src/templates/__tests__/GridTemplate.tsx
+++ src/templates/__tests__/GridTemplate.tsx
@@ -3,33 +3,49 @@ import { createStore } from 'redux';
 import rootReducer from 'reducers';
 import { fakeItemsData } from 'testUtils/fakers';
 import * as actions from 'actions';
+import { ItemVariants, Item } from 'commonTypes';
+import { RoutesPaths } from 'routes';
 import GridTemplate from '../GridTemplate/GridTemplate';
 
-const renderGridTemplate = (path, pageType) =>
+const fakeStore = createStore(rootReducer, {
+  userID: 'fake-id',
+  isLoading: false,
+  ...fakeItemsData,
+});
+
+const renderGridTemplate = (path?: RoutesPaths, pageType?: ItemVariants) =>
   render(
     <GridTemplate>
       <></>
     </GridTemplate>,
-    { path, pageType, store: createStore(rootReducer, fakeItemsData) },
+    {
+      path,
+      pageType,
+      store: fakeStore,
+    },
   );
 
 const getAllByHeadingRole = () => screen.getAllByRole('heading');
 const getByNewItemBar = () => screen.getByTestId('new-item-bar');
 const getAllByButtonRole = () => screen.getAllByRole('button');
-const getByTitlePlaceholderText = () => screen.queryByPlaceholderText(/title/i);
-const getByNewItemBarContentTextarea = () => screen.queryByTestId('new-item-bar-content-textarea');
+const getByTitlePlaceholderText = () => screen.getByPlaceholderText(/title/i);
+const getByNewItemBarContentTextarea = () => screen.getByTestId('new-item-bar-content-textarea');
 const getByAddNoteTextButton = () => screen.getByRole('button', { name: /add note/i });
 
 const fakeNewNoteData = { id: 'fake-note-id' };
 
 const mockAddItemAction = () =>
-  jest.spyOn(actions, 'addItem').mockImplementation((itemType, itemContent) => ({
-    type: 'ADD_ITEM_SUCCESS',
-    payload: {
-      itemType,
-      data: { ...fakeNewNoteData, ...itemContent },
-    },
-  }));
+  jest
+    .spyOn(actions, 'addItem')
+    // eslint-disable-next-line
+    // @ts-ignore
+    .mockImplementation((itemType: ItemVariants, itemContent: Item) => ({
+      type: 'ADD_ITEM_SUCCESS',
+      payload: {
+        itemType,
+        data: { ...fakeNewNoteData, ...itemContent },
+      },
+    }));
 
 const transformTranslateHundredPercent = 'transform: translate(100%)';
 const transformTranslateZero = 'transform: translate(0)';
@@ -39,7 +55,11 @@ const fakeNoteItemInputs = {
   content: 'test note content',
 };
 
-const getState = (store, itemType) => () => store.getState()?.[itemType];
+type Store = {
+  getState: () => Partial<typeof fakeItemsData>;
+};
+
+const getState = (store: Store, itemType: ItemVariants) => () => store.getState()[itemType];
 
 describe('<GridTemplate />', () => {
   it.each(getPairOfPathsAndPageTypes())(
@@ -109,7 +129,13 @@ describe('<GridTemplate />', () => {
 
     const { store } = renderGridTemplate();
 
-    expect(store.getState()?.notes).toHaveLength(4);
+    /**
+     * @todo remove this comments after add types to the Store
+     */
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    expect(store.getState().notes!).toHaveLength(4);
 
     // open sidebar form
     const [toggleNewItemBarButton] = getAllByButtonRole();
@@ -122,6 +148,11 @@ describe('<GridTemplate />', () => {
     // submit the note form
     await waitFor(() => userEvent.click(getByAddNoteTextButton()));
 
+    /**
+     * @todo remove this comments after add types to the Store
+     */
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     const getStateNotes = getState(store, 'notes');
 
     expect(getStateNotes()).toHaveLength(5);
@@ -130,9 +161,9 @@ describe('<GridTemplate />', () => {
         expect.objectContaining({
           ...fakeNewNoteData,
           ...fakeNoteItemInputs,
-          articleUrl: expect.any(String),
-          created: expect.any(String),
-          twitterName: expect.any(String),
+          articleUrl: expect.any(String) as string,
+          created: expect.any(String) as string,
+          twitterName: expect.any(String) as string,
         }),
       ]),
     );
