062eb5a98f73e8800a3855961c80db1f4ff9d15d
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-05-27 17:50:11 +0200

    refactor: move firebase setup to separate file

--- src/services/core/index.js
+++ src/services/core/index.js
@@ -1,13 +1,1 @@
-/* eslint-disable import/no-duplicates */
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-import config from './config';
-
-firebase.initializeApp(config);
-
-export const auth = firebase.auth();
-export const db = firebase.firestore();
-export const {
-  FieldValue: { serverTimestamp },
-} = firebase.firestore;
+export * from './setup';
---
+++ src/services/core/setup.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-duplicates */
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import config from './config';
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const db = firebase.firestore();
+export const {
+  FieldValue: { serverTimestamp },
+} = firebase.firestore;
