76cefa08bd6e6c71940a6a9c14f8ae779d4d0d90
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-05-29 11:44:12 +0200

    refactor: change types in type guards

--- src/utils/guards.ts
+++ src/utils/guards.ts
@@ -1,17 +1,10 @@
-import { Item, ArticleItem, TwitterItem, Modify } from 'commonTypes';
-import { ServiceItem } from '../services/servicesTypes';
+import { Item, ArticleItem, TwitterItem } from 'commonTypes';
+import { DocumentItem } from '../services/servicesTypes';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type ServiceItemWithAnyTypeCreatedProp = Modify<ServiceItem, { created: any }>;
-
-export const isArticleItem = (
-  obj: Item | ServiceItemWithAnyTypeCreatedProp,
-): obj is ArticleItem => {
+export const isArticleItem = (obj: Item | DocumentItem): obj is ArticleItem => {
   return typeof obj === 'object' && Boolean(obj.articleUrl) && !obj.twitterName;
 };
 
-export const isTwitterItem = (
-  obj: Item | ServiceItemWithAnyTypeCreatedProp,
-): obj is TwitterItem => {
+export const isTwitterItem = (obj: Item | DocumentItem): obj is TwitterItem => {
   return typeof obj === 'object' && !obj.articleUrl && Boolean(obj.twitterName);
 };
