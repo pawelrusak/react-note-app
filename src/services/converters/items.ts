6417379c7c5c0d189a5975ce2124b6152dd2ae12
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-05-29 11:05:17 +0200

    refactor: remove old converter

--- src/services/converters/items.ts
+++ src/services/converters/items.ts
@@ -1,42 +1,8 @@
 import firebase from 'firebase/app';
-import { Item, Modify } from 'commonTypes';
+import { Modify } from 'commonTypes';
 import { isArticleItem, isTwitterItem } from 'utils/guards';
-import {
-  QueryDocumentSnapshot,
-  Timestamp,
-  QuerySnapshot,
-  ServiceItem,
-  DocumentItem,
-} from '../servicesTypes';
+import { ServiceItem, DocumentItem } from '../servicesTypes';
 import { serverTimestamp } from '../core';
-
-type RestItemData =
-  | { articleUrl: string; twitterName: never }
-  | { articleUrl: never; twitterName: string }
-  | { articleUrl: never; twitterName: never };
-
-export const convertQuerySnapshotItem = (item: QueryDocumentSnapshot): Item => {
-  const data = item.data() as Modify<ServiceItem, { created: Timestamp }>;
-
-  const dataItem: RestItemData = {} as RestItemData;
-
-  if (isArticleItem(data)) {
-    dataItem.articleUrl = data.articleUrl as string;
-  } else if (isTwitterItem(data)) {
-    dataItem.twitterName = data.twitterName as string;
-  }
-
-  return {
-    id: item.id,
-    title: data.title,
-    content: data.content,
-    created: data.created.toDate(),
-    ...dataItem,
-  };
-};
-
-export const convertQuerySnapshot = (querySnapshot: QuerySnapshot): Item[] =>
-  querySnapshot.docs.map(convertQuerySnapshotItem);
 
 export const itemConverter = {
   toFirestore(data: DocumentItem): firebase.firestore.DocumentData {
@@ -52,7 +18,10 @@ export const itemConverter = {
     snapshot: firebase.firestore.QueryDocumentSnapshot,
     options: firebase.firestore.SnapshotOptions,
   ): DocumentItem {
-    const data = snapshot.data(options) as Modify<ServiceItem, { created: Timestamp }>;
+    const data = snapshot.data(options) as Modify<
+      ServiceItem,
+      { created: firebase.firestore.Timestamp }
+    >;
 
     const documentItem: DocumentItem = {
       id: snapshot.id,
