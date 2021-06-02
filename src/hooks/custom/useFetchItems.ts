524f68088c93672671851b42e0a72966a82bc7ad
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-06-02 22:44:35 +0200

    refactor: simplify fetch items hook

--- src/hooks/custom/useFetchItems.ts
+++ src/hooks/custom/useFetchItems.ts
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchItems } from 'actions';
@@ -6,18 +5,12 @@ import { ItemVariants } from 'commonTypes';
 import { RootState } from 'reducers';
 
 export const useFetchItems = <T extends ItemVariants>(itemType: T): RootState[T] => {
-  // eslint-disable-next-line
   const items = useSelector((state: RootState) => state[itemType]);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchTwitters = async () => {
-      await dispatch(fetchItems(itemType));
-    };
+    dispatch(fetchItems(itemType));
+  }, [dispatch, itemType]);
 
-    fetchTwitters();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return items ?? [];
+  return items;
 };
