718017c867c7f643de7ea5b6ae49037e3ef8c1de
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-06-02 17:36:17 +0200

    refactor: convert action creators to TypeScript

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,9 +1,12 @@
+import { Dispatch } from 'redux';
+import { Item, ItemVariants, Modify } from 'commonTypes';
 import {
   authenticateUser,
   fetchItems as fetchRemoteItems,
   removeItem as removeRemoteItems,
   addItem as addRemoteItems,
 } from 'services';
+import type { RootState } from 'reducers';
 
 export const ADD_ITEM_REQUEST = 'ADD_ITEM_REQUEST';
 export const ADD_ITEM_SUCCESS = 'ADD_ITEM_SUCCESS';
@@ -21,12 +24,14 @@ export const FETCH_REQUEST = 'FETCH_REQUEST';
 export const FETCH_SUCCESS = 'FETCH_SUCCESS';
 export const FETCH_FAILURE = 'FETCH_FAILURE';
 
+type GetState = () => RootState;
+
 /**
  * You can find the original code in the link below
  *
  * @see {@link https://github.com/eduwebpl/kurs-react-w-praktyce/blob/a54a88872b762b772252d26b0373ae3b66248895/06/src/actions/index.js#L14-L30}
  */
-export const fetchItems = (itemType) => (dispatch, getState) => {
+export const fetchItems = (itemType: ItemVariants) => (dispatch: Dispatch, getState: GetState) => {
   dispatch({ type: FETCH_REQUEST });
 
   return fetchRemoteItems({ type: itemType, userID: getState().userID })
@@ -44,7 +49,7 @@ export const fetchItems = (itemType) => (dispatch, getState) => {
     });
 };
 
-export const removeItem = (itemType, id) => (dispatch) => {
+export const removeItem = (itemType: ItemVariants, id: string) => (dispatch: Dispatch) => {
   dispatch({ type: REMOVE_ITEM_REQUEST });
 
   removeRemoteItems(id)
@@ -65,34 +70,36 @@ export const removeItem = (itemType, id) => (dispatch) => {
  *
  * @see {@link https://github.com/eduwebpl/kurs-react-w-praktyce/blob/1144f53bbb0014406bb97cc03801b0bcf4a3ed97/06/src/actions/index.js#L82-L104}
  */
-export const addItem = (itemType, itemContent) => (dispatch, getState) => {
-  dispatch({ type: ADD_ITEM_REQUEST });
+export const addItem =
+  (itemType: ItemVariants, itemContent: Modify<Item, { created: undefined }>) =>
+  (dispatch: Dispatch, getState: GetState) => {
+    dispatch({ type: ADD_ITEM_REQUEST });
 
-  addRemoteItems({
-    type: itemType,
-    userID: getState().userID,
-    ...itemContent,
-  })
-    .then(({ data }) => {
-      dispatch({
-        type: ADD_ITEM_SUCCESS,
-        payload: {
-          itemType,
-          data,
-        },
-      });
+    addRemoteItems({
+      type: itemType,
+      userID: getState().userID,
+      ...itemContent,
     })
-    .catch(() => {
-      dispatch({ type: ADD_ITEM_FAILURE });
-    });
-};
+      .then(({ data }) => {
+        dispatch({
+          type: ADD_ITEM_SUCCESS,
+          payload: {
+            itemType,
+            data,
+          },
+        });
+      })
+      .catch(() => {
+        dispatch({ type: ADD_ITEM_FAILURE });
+      });
+  };
 
 /**
  * Simulate original course code by Firebase
  *
  * @see {@link https://github.com/eduwebpl/kurs-react-w-praktyce/blob/54f2da267adf1c60ba8428ae64db8e1d42ec6c57/06/src/actions/index.js#L31-L47}
  */
-export const authenticate = (email, password) => (dispatch) => {
+export const authenticate = (email: string, password: string) => (dispatch: Dispatch) => {
   dispatch({ type: AUTH_REQUEST });
 
   return (
@@ -100,7 +107,7 @@ export const authenticate = (email, password) => (dispatch) => {
       .then((payload) => {
         dispatch({ type: AUTH_SUCCESS, payload });
       })
-      // eslint-disable-next-line no-unused-vars
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       .catch((err) => {
         dispatch({ type: AUTH_FAILURE });
       })
