dbfa78b03c793b2a913d72906f5058c58db70e8a
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-06-02 16:40:42 +0200

    refactor: convert root reducer to TypeScript

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,3 +1,5 @@
+import firebase from 'firebase/app';
+import { Item, NoteItem, ArticleItem, TwitterItem, ItemVariants } from 'commonTypes';
 import {
   ADD_ITEM_SUCCESS,
   // REMOVE_ITEM_REQUEST,
@@ -9,7 +11,18 @@ import {
   FETCH_FAILURE,
 } from 'actions';
 
+export type RootState = {
+  readonly notes: NoteItem[];
+  readonly twitters: TwitterItem[];
+  readonly articles: ArticleItem[];
+  readonly userID: string | null;
+  readonly isLoading: boolean;
+};
+
 const initialState = {
+  notes: [],
+  twitters: [],
+  articles: [],
   /**
    * You can find the original code of the course in the link below.
    *
@@ -17,11 +30,19 @@ const initialState = {
    *
    * @see {@link https://github.com/eduwebpl/kurs-react-w-praktyce/blob/ce05514413ce0d022623870c7327ca4fe7dea0d5/06/src/reducers/index.js#L4}
    */
-  userID: process.env.REACT_APP_TEMPORARY_USER_ID,
+  userID: process.env.REACT_APP_TEMPORARY_USER_ID as string,
   isLoading: false,
 };
 
-const rootReducer = (state = initialState, action) => {
+type RootAction =
+  | { type: typeof FETCH_REQUEST }
+  | { type: typeof FETCH_SUCCESS; payload: { itemType: ItemVariants; data: Item[] } }
+  | { type: typeof FETCH_FAILURE }
+  | { type: typeof REMOVE_ITEM_SUCCESS; payload: { itemType: ItemVariants; id: string } }
+  | { type: typeof ADD_ITEM_SUCCESS; payload: { itemType: ItemVariants; data: Item } }
+  | { type: typeof AUTH_SUCCESS; payload: firebase.auth.UserCredential };
+
+const rootReducer = (state: RootState = initialState, action: RootAction) => {
   switch (action.type) {
     case FETCH_REQUEST:
       return {
@@ -42,7 +63,7 @@ const rootReducer = (state = initialState, action) => {
     case AUTH_SUCCESS:
       return {
         ...state,
-        userID: action.payload.user.uid,
+        userID: action.payload.user?.uid ?? null,
       };
     case ADD_ITEM_SUCCESS:
       return {
@@ -53,7 +74,9 @@ const rootReducer = (state = initialState, action) => {
       return {
         ...state,
         [action.payload.itemType]: [
-          ...state[action.payload.itemType].filter((item) => item.id !== action.payload.id),
+          ...(state[action.payload.itemType] as Item[]).filter(
+            (item) => item.id !== action.payload.id,
+          ),
         ],
       };
     default:
