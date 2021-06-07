061209aec89d2a5b160eee2ee94940385710561a
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-06-07 21:14:57 +0200

    refactor: change type for example data of stories

--- src/store/__mocks__/index.js
+++ src/store/__mocks__/index.js
@@ -1,10 +1,11 @@
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import { getEarlierDateOfDay } from 'utils';
 
 const sampleNote = {
   id: '8885d2d6-b081-4342-8232-e889affa9d93',
   title: 'My best note ever',
-  created: '3 days',
+  created: getEarlierDateOfDay(3),
   content:
     'Miles Dewey Davis III (May 26, 1926 - September 28, 1991) was an American jazz trumpeter, bandleader, and composer.',
 };
