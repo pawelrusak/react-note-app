35fa1bdbb630f68a98409db9b3488e4659b9254f
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-06-07 21:37:00 +0200

    chore: simplify stories of <DetailsTemplate />

--- src/templates/DetailsTemplate/DetailsTemplate.stories.tsx
+++ src/templates/DetailsTemplate/DetailsTemplate.stories.tsx
@@ -1,7 +1,6 @@
 import { Meta, Story } from '@storybook/react';
 import StoryRouter from 'storybook-react-router';
 import * as CardStories from 'components/molecules/Card/Card.stories';
-import { getEarlierDateOfDay } from 'utils';
 import DetailsTemplate, { DetailsTemplateProps } from './DetailsTemplate';
 
 export default {
@@ -12,14 +11,9 @@ export default {
 
 const Template: Story<DetailsTemplateProps> = (args) => <DetailsTemplate {...args} />;
 
-/**
- * @todo remove 'created' property in arguments composition after change his type in storied of Card in each DetailsTemplate stories
- */
 export const Note = Template.bind({});
 Note.args = {
   ...CardStories.Note.args,
-
-  created: getEarlierDateOfDay(3),
 };
 Note.parameters = {
   pageContext: 'notes',
@@ -28,7 +22,6 @@ Note.parameters = {
 export const Twitter = Template.bind({});
 Twitter.args = {
   ...CardStories.Twitter.args,
-  created: getEarlierDateOfDay(3),
 };
 Twitter.parameters = {
   pageContext: 'twitters',
@@ -37,7 +30,6 @@ Twitter.parameters = {
 export const Article = Template.bind({});
 Article.args = {
   ...CardStories.Article.args,
-  created: getEarlierDateOfDay(3),
 };
 Article.parameters = {
   pageContext: 'articles',
