30437bbb2b3bddc965c9519076092e98dc2166ed
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-06-08 21:31:35 +0200

    test: convert the helpers of tests to TypeScript

---
+++ src/testUtils/helpers.ts
@@ -0,0 +1,9 @@
+import { routes } from 'routes';
+import { stripSlashPrefix } from 'utils';
+
+export const getPairOfPathsAndPageTypes = () =>
+  [
+    [routes.notes, stripSlashPrefix(routes.notes) as 'notes'],
+    [routes.twitters, stripSlashPrefix(routes.twitters) as 'twitters'],
+    [routes.articles, stripSlashPrefix(routes.articles) as 'articles'],
+  ] as const;
