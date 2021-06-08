b3136076bcb3e9e12287863242042d699d0b4aa7
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-06-08 23:22:07 +0200

    test: convert renderWithRouter() to TypeScript

---
+++ src/testUtils/renders/renderWithRouter.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { render as rtlRender } from '@testing-library/react';
+import type { RenderOptions as RtlRenderOptions } from '@testing-library/react';
+
+type RenderOptions = { path?: string } & RtlRenderOptions;
+
+export const renderWithRouter = (
+  ui: React.ReactElement,
+  { path = '/', ...renderOptions }: RenderOptions = {},
+) => {
+  window.history.pushState({}, 'Test page', path);
+
+  const Wrapper: React.FC = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
+
+  Wrapper.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+
+  return rtlRender(ui, {
+    wrapper: Wrapper,
+    ...renderOptions,
+  });
+};
