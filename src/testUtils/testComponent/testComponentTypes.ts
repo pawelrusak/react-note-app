a107f4b96ce7bfc03315c0192c83a55b8b8aa5ec
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-06-11 14:32:45 +0200

    chore: create types for test component builder

--- src/testUtils/testComponent/testComponentTypes.ts
+++ src/testUtils/testComponent/testComponentTypes.ts
@@ -1,1 +1,25 @@
+import type { RenderResult } from '@testing-library/react';
+
 export type TestQueries = () => HTMLElement | null;
+
+export type LikeRender = (...args: unknown[]) => RenderResult;
+
+export interface Negotiable {
+  not: this;
+}
+
+export interface BeInTheDocumentMatchable {
+  toBeInTheDocument: (name: string, element: TestQueries) => this;
+}
+
+export interface AttributeTestable {
+  withAttribute: (attr: string, value: unknown) => this;
+}
+
+export interface Getable {
+  get(): unknown[];
+}
+
+export interface Renderable {
+  getRenderComponent: LikeRender;
+}
