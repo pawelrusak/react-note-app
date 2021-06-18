ce91f47696528279d99e73e09ef8518300b224c4
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-06-18 23:04:04 +0200

    refactor: split state selectors

--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -1,9 +1,2 @@
-import type { ItemVariants, Item } from '~/commonTypes';
-import type { RootState } from '~/reducers';
-
-export const itemByTypeAndIDSelector =
-  <T extends ItemVariants>(itemType: T, itemID: string) =>
-  (store: RootState) =>
-    (store.items[itemType] as Item[] | undefined)?.find((item) => item.id === itemID);
-
-export const userIDSelector = ({ auth }: RootState) => auth.userID;
+export * from './auth';
+export * from './items';
