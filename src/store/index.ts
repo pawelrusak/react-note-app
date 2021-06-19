4227cd7d6121e1c0d1e89a5eef824be4048f9388
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-06-19 15:14:45 +0200

    refactor: move the type aliases of the states

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,4 +7,8 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
+export type RootState = ReturnType<typeof store.getState>;
+export type { AuthState } from './auth/authReducer';
+export type { ItemsState } from './items/itemsReducer';
+
 export default store;
