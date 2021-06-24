c16dfebec3b239de5b248b97293f9e278a0ad24b
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-06-24 17:36:39 +0200

    test: mock fetch items service

--- src/services/__mocks__/index.ts
+++ src/services/__mocks__/index.ts
@@ -1,6 +1,20 @@
+import { fakeItemsData } from 'testUtils/fakers';
+
+import type { ItemVariants } from '~/commonTypes';
+
 export const removeItem = async () => {
   try {
     return Promise.resolve();
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+export const fetchItems = async ({ type }: { type: ItemVariants }) => {
+  try {
+    const data = fakeItemsData[type];
+
+    return Promise.resolve({ data });
   } catch (error) {
     return Promise.reject(error);
   }
