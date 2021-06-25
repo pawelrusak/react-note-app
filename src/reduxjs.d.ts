f4d2022cb1e23963d724e2e9071e3546d666db24
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-06-25 19:01:10 +0200

    refactor: create type for items payload action

--- src/reduxjs.d.ts
+++ src/reduxjs.d.ts
@@ -1,7 +1,10 @@
+import { PayloadAction } from '@reduxjs/toolkit';
+
 import auth, { AuthAction } from '~/store/auth/authReducer';
 import items, { ItemsAction } from '~/store/items/itemsReducer';
 
 import type { Store } from '@reduxjs/toolkit';
+import type { ItemVariants } from '~/commonTypes';
 
 declare module '@reduxjs/toolkit' {
   export type RootAction = AuthAction & ItemsAction;
@@ -12,4 +15,18 @@ declare module '@reduxjs/toolkit' {
   };
 
   export type AppStore = Store<RootState, RootAction>;
+
+  export type AppItemsPayloadAction<
+    P = void,
+    T extends string = string,
+    M = never,
+    E = never,
+  > = PayloadAction<
+    {
+      itemType: ItemVariants;
+    } & P,
+    T,
+    M,
+    E
+  >;
 }
