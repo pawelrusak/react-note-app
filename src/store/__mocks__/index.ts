cb9eaeeb8ff544905530c46c5ccb19d41fc6f9ae
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-06-29 22:51:30 +0200

    test: update tests for string type of "created"

--- src/store/__mocks__/index.ts
+++ src/store/__mocks__/index.ts
@@ -9,7 +9,7 @@ import type { RootState } from '~/store';
 const sampleNote = {
   id: '8885d2d6-b081-4342-8232-e889affa9d93',
   title: 'My best note ever',
-  created: getEarlierDateOfDay(3),
+  created: getEarlierDateOfDay(3).toISOString(),
   content:
     'Miles Dewey Davis III (May 26, 1926 - September 28, 1991) was an American jazz trumpeter, bandleader, and composer.',
 };
