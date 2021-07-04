16b632e949d9e536ffa084fa6a55659cad6856f8
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-07-04 22:10:12 +0200

    chore: add disabled prop to stories of <Button />

--- src/components/atoms/Button/Button.stories.tsx
+++ src/components/atoms/Button/Button.stories.tsx
@@ -7,11 +7,14 @@ export default {
   component: Button,
 } as Meta;
 
-const Template: Story<ButtonProps> = (args) => <Button {...args}>Hello, World!</Button>;
+type StoryButtonProps = ButtonProps & React.ComponentPropsWithoutRef<'button'>;
+
+const Template: Story<StoryButtonProps> = (args) => <Button {...args}>Hello, World!</Button>;
 
 export const Note = Template.bind({});
 Note.args = {
   activecolor: 'notes',
+  disabled: false,
 };
 Note.argTypes = {
   activecolor: {
@@ -29,6 +32,7 @@ Note.argTypes = {
 
 export const Twitter = Template.bind({});
 Twitter.args = {
+  ...Note.argTypes,
   activecolor: 'twitters',
 };
 Twitter.argTypes = {
@@ -37,6 +41,7 @@ Twitter.argTypes = {
 
 export const Article = Template.bind({});
 Article.args = {
+  ...Note.argTypes,
   activecolor: 'articles',
 };
 Article.argTypes = {
