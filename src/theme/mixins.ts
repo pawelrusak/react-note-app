8c3604c2735ce0a752eca9124f8779086348b331
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-07-04 21:58:08 +0200

    refactor: create SC mixins for lighten colors

--- src/theme/mixins.ts
+++ src/theme/mixins.ts
@@ -1,3 +1,4 @@
+import { lighten } from 'polished';
 import { css } from 'styled-components';
 
 import type { ItemVariants } from '~/commonTypes';
@@ -7,4 +8,15 @@ export type ActiveColorArgs = { readonly activecolor?: ItemVariants };
 export const activecolor = css<ActiveColorArgs>`
   background-color: ${(props) =>
     props.activecolor ? props.theme[props.activecolor] : props.theme.notes};
+`;
+
+export type LightenActiveColorArgs = ActiveColorArgs;
+
+export const lightenActiveColor = css<LightenActiveColorArgs>`
+  background-color: ${(props) =>
+    lighten(0.1, props.activecolor ? props.theme[props.activecolor] : props.theme.notes)};
+`;
+
+export const lightenBlackText = css`
+  color: ${({ theme }) => lighten(0.25, theme.black)};
 `;
