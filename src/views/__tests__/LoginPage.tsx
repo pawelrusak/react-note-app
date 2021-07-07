b4d6cc4706a5ef74c0d5cc7ec8bbf456f0bc62e3
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-07-07 02:08:44 +0200

    test: add the tests for <LoginPage /> validation

--- src/views/__tests__/LoginPage.tsx
+++ src/views/__tests__/LoginPage.tsx
@@ -53,4 +53,40 @@ describe('<LoginPage />', () => {
     // redirect to the home page
     await waitFor(() => expect(queryByFakeHomePage()).toBeInTheDocument());
   });
+
+  it('initially, the form should not contain any errors and button should be enable', () => {
+    renderLoginPage();
+
+    expect(getByLoginButton()).toBeEnabled();
+    expect(getByLoginPlaceholderText()).toBeValid();
+    expect(getByPasswordPlaceholderText()).toBeValid();
+  });
+
+  it('disable the button after submitting an invalid form and enable it after fixing all form errors', async () => {
+    renderLoginPage();
+
+    // attempt to submit an invalid form
+    await waitFor(() => userEvent.click(getByLoginButton()));
+
+    // the submit button should now be disabled and the form fields should be invalid
+    expect(getByLoginButton()).toBeDisabled();
+    expect(getByLoginPlaceholderText()).toBeInvalid();
+    expect(getByPasswordPlaceholderText()).toBeInvalid();
+
+    userEvent.type(getByLoginPlaceholderText(), 'correct@example.email');
+
+    // the submit button should not be enable yet, it is waiting for all form fields to be valid
+    await waitFor(() => expect(getByLoginButton()).toBeDisabled());
+
+    expect(getByLoginPlaceholderText()).toBeValid();
+    expect(getByPasswordPlaceholderText()).toBeInvalid();
+
+    userEvent.type(getByPasswordPlaceholderText(), 'correct-password');
+
+    // when all form fields are valid then enable the submit button
+    await waitFor(() => expect(getByLoginButton()).toBeEnabled());
+
+    expect(getByLoginPlaceholderText()).toBeValid();
+    expect(getByPasswordPlaceholderText()).toBeValid();
+  });
 });
