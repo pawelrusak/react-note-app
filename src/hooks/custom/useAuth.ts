3cf80232d404b7ae30582bc500f2649997732ddf
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-07-12 20:07:10 +0200

    refactor: add logout action to useAuth hook

--- src/hooks/custom/useAuth.ts
+++ src/hooks/custom/useAuth.ts
@@ -2,24 +2,24 @@ import { RootDispatch } from '@reduxjs/toolkit';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { userIDSelector } from '~/store/auth/authSelectors';
-import {
-  authenticate as authenticateAction,
-  register as registerAction,
-} from '~/store/auth/authSlice';
+import * as actions from '~/store/auth/authSlice';
 
 export const useAuth = () => {
   const userID = useSelector(userIDSelector);
   const dispatch = useDispatch<RootDispatch>();
 
   const authenticate = async (email: string, password: string) =>
-    dispatch(authenticateAction({ email, password })).unwrap();
+    dispatch(actions.authenticate({ email, password })).unwrap();
 
   const register = (email: string, password: string) =>
-    dispatch(registerAction({ email, password })).unwrap();
+    dispatch(actions.register({ email, password })).unwrap();
+
+  const logout = () => dispatch(actions.logout()).unwrap();
 
   return {
     userID,
     authenticate,
     register,
+    logout,
   };
 };
