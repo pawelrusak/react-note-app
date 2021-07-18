891c917532d62d27607b4e4e6c97db3c451512b8
Paweł Rusak <78496242+pawelrusak@users.noreply.github.com>
2021-07-18 17:48:21 +0200

    feat: protect URL paths for non auth pages (#76)
    * test: write test for <PrivateRoute />
    
    * refactor: create <PrivateRoute />
    
    * feat: protect url paths for non auth pages
    
    protect url paths that do not belong to entry pages

--- src/views/Root.tsx
+++ src/views/Root.tsx
@@ -8,6 +8,7 @@ import Notes from './Notes/Notes';
 import RegisterPage from './RegisterPage/RegisterPage';
 import Twitters from './Twitters/Twitters';
 import { routes } from '~/routes';
+import PrivateRoute from '~/routes/PrivateRoute';
 import store from '~/store';
 import MainTemplate from '~/templates/MainTemplate/MainTemplate';
 
@@ -18,13 +19,13 @@ const Root = () => (
         <Switch>
           <Route exact path={routes.login} component={LoginPage} />
           <Route exact path={routes.register} component={RegisterPage} />
-          <Route exact path={routes.home} render={() => <Redirect to="/notes" />} />
-          <Route exact path={routes.notes} component={Notes} />
-          <Route path={routes.note} component={DetailsPage} />
-          <Route exact path={routes.articles} component={Articles} />
-          <Route path={routes.article} component={DetailsPage} />
-          <Route exact path={routes.twitters} component={Twitters} />
-          <Route path={routes.twitter} component={DetailsPage} />
+          <PrivateRoute exact path={routes.home} render={() => <Redirect to="/notes" />} />
+          <PrivateRoute exact path={routes.notes} component={Notes} />
+          <PrivateRoute path={routes.note} component={DetailsPage} />
+          <PrivateRoute exact path={routes.articles} component={Articles} />
+          <PrivateRoute path={routes.article} component={DetailsPage} />
+          <PrivateRoute exact path={routes.twitters} component={Twitters} />
+          <PrivateRoute path={routes.twitter} component={DetailsPage} />
         </Switch>
       </MainTemplate>
     </BrowserRouter>
