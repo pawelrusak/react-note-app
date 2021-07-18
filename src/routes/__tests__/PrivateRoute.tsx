e7707583caf887d58a02d02cee22bfbbbfc6c6da
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-07-18 17:07:40 +0200

    test: write test for <PrivateRoute />

---
+++ src/routes/__tests__/PrivateRoute.tsx
@@ -0,0 +1,30 @@
+import { Switch } from 'react-router-dom';
+import { render } from 'testUtils';
+import { fakeStateWithNotLoggedInUser } from 'testUtils/fakers';
+
+import { routes } from '../index';
+import PrivateRoute from '../PrivateRoute';
+import Notes from '~/views/Notes/Notes';
+
+describe('<PrivateRoute />', () => {
+  afterEach(() => {
+    // clean URL path
+    window.history.pushState({}, 'Test page', '/');
+  });
+
+  it('redirect to "/login" URL path when auth state does not have user logged in', () => {
+    expect(window.location.pathname).toBe('/');
+
+    render(
+      <Switch>
+        <PrivateRoute path={routes.notes} component={Notes} />
+      </Switch>,
+      {
+        path: routes.notes,
+        initialState: fakeStateWithNotLoggedInUser,
+      },
+    );
+
+    expect(window.location.pathname).toBe(routes.login);
+  });
+});
