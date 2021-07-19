01a468725312f680a5145f0c29361f40dc5d84d1
Paweł Rusak <78496242+pawelrusak@users.noreply.github.com>
2021-07-19 23:20:35 +0200

    refactor: create <DateInfo /> (#80)
    * chore: add dayjs package to dependencies
    
    * refactor: create <DateInfo />
    
    * chore: create the stories for the <DateInfo />

---
+++ src/components/atoms/DateInfo/DateInfo.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta, Story } from '@storybook/react';
+
+import DateInfo, { DateInfoProps } from './DateInfo';
+import { getEarlierDateOfDay } from '~/utils';
+
+export default {
+  title: 'Atoms/DateInfo',
+  component: DateInfo,
+} as Meta;
+
+const Template: Story<DateInfoProps> = (props) => <DateInfo {...props} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  ISOStringDate: getEarlierDateOfDay(3).toISOString(),
+};
+
+export const Render = Template.bind({});
+Render.args = {
+  ...Default.args,
+  render: ({ date }) => (
+    <>
+      <mark>React node text</mark> - {date}
+    </>
+  ),
+};
+
+export const Format = Template.bind({});
+Format.args = {
+  ...Default.args,
+  format: true,
+};
