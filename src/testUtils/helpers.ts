5c144f8f1a621d69a5afbe0eb1aad4a5d73f9aa0
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-07-28 00:34:45 +0200

    test: create getRandomNaturalNumber() test helper

--- src/testUtils/helpers.ts
+++ src/testUtils/helpers.ts
@@ -7,3 +7,17 @@ export const getPairOfPathsAndPageTypes = () =>
     [routes.twitters, stripSlashPrefix(routes.twitters) as 'twitters'],
     [routes.articles, stripSlashPrefix(routes.articles) as 'articles'],
   ] as const;
+
+export const getRandomNaturalNumber = (max = 10, min = 1) => {
+  if (!Number.isInteger(max) || !Number.isInteger(min)) {
+    throw RangeError('The given values have to been integer');
+  }
+
+  if (min >= max) {
+    throw RangeError(
+      'The minimum values specified must be greater than and not equal to the maximum value',
+    );
+  }
+
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
+};
--- src/testUtils/index.ts
+++ src/testUtils/index.ts
@@ -1,5 +1,5 @@
 export { default as userEvent } from '@testing-library/user-event';
 export * from '@testing-library/react';
 export { render, renderWithRouter, rtlRender } from './renders';
-export { getPairOfPathsAndPageTypes } from './helpers';
+export { getPairOfPathsAndPageTypes, getRandomNaturalNumber } from './helpers';
 export { testComponent } from './testComponent';
