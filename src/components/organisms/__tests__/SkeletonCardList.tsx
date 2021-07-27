4efcbc5a9b137f55f0949587b4525be6dcd4d4db
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-07-28 00:40:53 +0200

    test: write tests for the <SkeletonCardList />

---
+++ src/components/organisms/__tests__/SkeletonCardList.tsx
@@ -0,0 +1,28 @@
+import { render, screen, getRandomNaturalNumber } from 'testUtils';
+
+import SkeletonCardList from '../SkeletonCardList/SkeletonCardList';
+
+const getAllSkeletonCard = () => screen.getAllByTestId('skeleton-card');
+
+const renderSkeletonCardList = (length: number | undefined = undefined) =>
+  render(<SkeletonCardList length={length} />);
+
+/**
+ * I intentionally omit tests for other variants of the skeleton card list because they are
+ * temporary and will be deprecated in the future and to avoid testing implement details/
+ */
+describe('<SkeletonCardList />', () => {
+  it('render six cards in the list by default', () => {
+    renderSkeletonCardList();
+
+    expect(getAllSkeletonCard()).toHaveLength(6);
+  });
+
+  it('render given number of cards in the list', () => {
+    const CARDS_NUMBER = getRandomNaturalNumber();
+
+    renderSkeletonCardList(CARDS_NUMBER);
+
+    expect(getAllSkeletonCard()).toHaveLength(CARDS_NUMBER);
+  });
+});
