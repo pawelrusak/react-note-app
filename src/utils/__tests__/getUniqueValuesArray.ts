e560edbfeba2954c667a13f5da687ea7d614073e
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-07-27 23:00:43 +0200

    test: write tests for getUniqueValuesArray()

---
+++ src/utils/__tests__/getUniqueValuesArray.ts
@@ -0,0 +1,26 @@
+import { getUniqueValuesArray } from '../index';
+
+describe('getUniqueValuesArray', () => {
+  it('throws a range error if the given length is less than zero', () => {
+    expect(() => getUniqueValuesArray(-5)).toThrow(RangeError);
+  });
+
+  it('throws a range error if the given length is not integer', () => {
+    expect(() => getUniqueValuesArray(0.5)).toThrow(RangeError);
+  });
+
+  it('return an array of the given length', () => {
+    expect(getUniqueValuesArray(9)).toHaveLength(9);
+  });
+
+  it('return an array of unique values', () => {
+    const result = getUniqueValuesArray(20);
+
+    const set = new Set(result);
+    // eslint-disable-next-line
+    // @ts-expect-error
+    const arrayOfUniqueValues = [...set];
+
+    expect(result).toEqual(arrayOfUniqueValues);
+  });
+});
