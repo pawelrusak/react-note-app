f9a747b8e0d9ff606b45354c0f92b18e773019da
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-07-30 16:17:21 +0200

    refactor: create <CardList />

---
+++ src/components/organisms/CardList/CardList.tsx
@@ -0,0 +1,20 @@
+import SkeletonCardList from '~/components/organisms/SkeletonCardList/SkeletonCardList';
+import { useFetchItems } from '~/hooks';
+
+import type { ItemVariants } from '~/commonTypes';
+import type { ItemsState } from '~/store';
+
+type Children<T extends ItemVariants> = ({ data }: { data: ItemsState[T] }) => React.ReactNode;
+
+export type CardListProps<T extends ItemVariants> = {
+  readonly variant: T;
+  readonly children: Children<T>;
+};
+
+const CardList = <T extends ItemVariants>({ variant, children }: CardListProps<T>) => {
+  const { data, loading } = useFetchItems(variant);
+
+  return <>{loading ? <SkeletonCardList lighten /> : children({ data })}</>;
+};
+
+export default CardList;
