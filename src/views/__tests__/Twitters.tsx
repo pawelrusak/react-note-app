9a75d255b87170cfa93953d143804f45877cd269
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-08-03 21:39:56 +0200

    test: use shared test suite in <Twitters/> tests

--- src/views/__tests__/Twitters.tsx
+++ src/views/__tests__/Twitters.tsx
@@ -1,78 +1,18 @@
-import { render, screen, userEvent, waitFor, waitForElementToBeRemoved } from 'testUtils';
-import { fakeItemsData } from 'testUtils/fakers';
+import { render } from 'testUtils';
+import { fetchItemsTestSuite } from 'testUtils/sharedTests';
 
 import Twitters from '../Twitters/Twitters';
 import { routes } from '~/routes';
-import { stripSlashPrefix } from '~/utils';
 
 jest.mock('~/services');
 
 const renderTwitters = () =>
   render(<Twitters />, {
     path: routes.twitters,
-    pageType: stripSlashPrefix(routes.twitters) as 'twitters',
+    pageType: 'twitters',
   });
 
-const { twitters } = fakeItemsData;
-
-const firstTwitterTitle = twitters[0].title;
-const cardHeadingBarId = 'card-heading-bar';
-
-const getAllByCardHeadings = () => screen.getAllByTestId(cardHeadingBarId);
-const getByFirstTwitterTitle = () => screen.queryByText(firstTwitterTitle);
-const findAllByRemoveButtons = () => screen.findAllByRole('button', { name: /remove/i });
-const findAllByCardHeadings = () => screen.findAllByTestId(cardHeadingBarId);
-const findByFirstTwitterTitle = () => screen.findByText(firstTwitterTitle);
-const queryAllBySkeletonCard = () => screen.queryAllByTestId('skeleton-card');
-const queryByGridTemplateCounter = () => screen.queryByTestId('grid-template-counter');
-const queryByGridTemplateSkeletonCounter = () =>
-  screen.queryByTestId('grid-template-skeleton-counter');
-
-describe('<Twitters />', () => {
-  it('display <CardListSkeleton /> until fetch data', async () => {
-    renderTwitters();
-
-    expect(queryAllBySkeletonCard()).toHaveLength(6);
-
-    await waitForElementToBeRemoved(() => queryAllBySkeletonCard());
-
-    expect(queryAllBySkeletonCard()).toHaveLength(0);
-  });
-
-  it('display counter skeleton until fetch data', async () => {
-    renderTwitters();
-
-    expect(queryByGridTemplateSkeletonCounter()).toBeInTheDocument();
-
-    await waitForElementToBeRemoved(() => queryByGridTemplateSkeletonCounter());
-
-    expect(queryByGridTemplateSkeletonCounter()).not.toBeInTheDocument();
-    expect(queryByGridTemplateCounter()).toBeInTheDocument();
-  });
-
-  it('display the cards with data from store', async () => {
-    renderTwitters();
-
-    const cardsHeadings = await findAllByCardHeadings();
-
-    expect(cardsHeadings).toHaveLength(4);
-
-    for (let i = 0; i < twitters.length; i += 1) {
-      expect(cardsHeadings[i]).toHaveTextContent(twitters[i].title);
-    }
-  });
-
-  it('delete first card after clicking its the "remove" button', async () => {
-    renderTwitters();
-
-    expect(await findAllByCardHeadings()).toHaveLength(4);
-    expect(await findByFirstTwitterTitle()).toBeInTheDocument();
-
-    const [firstCardItemRemoveButton] = await findAllByRemoveButtons();
-
-    await waitFor(() => userEvent.click(firstCardItemRemoveButton));
-
-    expect(getAllByCardHeadings()).toHaveLength(3);
-    expect(getByFirstTwitterTitle()).not.toBeInTheDocument();
-  });
+fetchItemsTestSuite('<Twitters />', {
+  render: () => renderTwitters(),
+  variant: 'twitters',
 });
