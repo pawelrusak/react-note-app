a2082f03184b1299fe586bcc85b4af7d8fbf1aa7
Paweł Rusak <78496242+pawelrusak@users.noreply.github.com>
2021-08-03 21:22:44 +0200

    feat: fetch notes (#98)
    * test: create shared test suite to fetch items
    
    * test: create test of fetch note items for <Notes>
    
    * feat: fetch notes in <Notes />
    
    * chore: add loading story for <Notes /> stories

---
+++ src/testUtils/sharedTests.ts
@@ -0,0 +1,83 @@
+import {
+  screen,
+  render as renderWithProviders,
+  waitFor,
+  waitForElementToBeRemoved,
+  userEvent,
+} from 'testUtils';
+import { fakeItemsData } from 'testUtils/fakers';
+
+import type { ItemVariants } from '~/commonTypes';
+
+type FetchItemsTestSuiteConfigArgs = {
+  render(): ReturnType<typeof renderWithProviders>;
+  variant: ItemVariants;
+};
+
+export const fetchItemsTestSuite = (
+  testGroupName: string,
+  { render, variant }: FetchItemsTestSuiteConfigArgs,
+) => {
+  const itemsData = fakeItemsData[variant];
+  const firstItemsTitle = itemsData[0].title;
+
+  const cardHeadingBarId = 'card-heading-bar';
+  const getAllByCardHeadings = () => screen.getAllByTestId(cardHeadingBarId);
+  const getByFirstItemsTitle = () => screen.queryByText(firstItemsTitle);
+  const findAllByRemoveButtons = () => screen.findAllByRole('button', { name: /remove/i });
+  const findAllByCardHeadings = () => screen.findAllByTestId(cardHeadingBarId);
+  const findByFirstItemsTitle = () => screen.findByText(firstItemsTitle);
+  const queryAllBySkeletonCard = () => screen.queryAllByTestId('skeleton-card');
+  const queryByGridTemplateCounter = () => screen.queryByTestId('grid-template-counter');
+  const queryByGridTemplateSkeletonCounter = () =>
+    screen.queryByTestId('grid-template-skeleton-counter');
+
+  describe(testGroupName, () => {
+    it('display <CardListSkeleton /> until fetch data', async () => {
+      render();
+
+      expect(queryAllBySkeletonCard()).toHaveLength(6);
+
+      await waitForElementToBeRemoved(() => queryAllBySkeletonCard());
+
+      expect(queryAllBySkeletonCard()).toHaveLength(0);
+    });
+
+    it('display counter skeleton until fetch data', async () => {
+      render();
+
+      expect(queryByGridTemplateSkeletonCounter()).toBeInTheDocument();
+
+      await waitForElementToBeRemoved(() => queryByGridTemplateSkeletonCounter());
+
+      expect(queryByGridTemplateSkeletonCounter()).not.toBeInTheDocument();
+      expect(queryByGridTemplateCounter()).toBeInTheDocument();
+    });
+
+    it('display the cards with data from store', async () => {
+      render();
+
+      const cardsHeadings = await findAllByCardHeadings();
+
+      expect(cardsHeadings).toHaveLength(4);
+
+      for (let i = 0; i < itemsData.length; i += 1) {
+        expect(cardsHeadings[i]).toHaveTextContent(itemsData[i].title);
+      }
+    });
+
+    it('delete first card after clicking its the "remove" button', async () => {
+      render();
+
+      expect(await findAllByCardHeadings()).toHaveLength(4);
+      expect(await findByFirstItemsTitle()).toBeInTheDocument();
+
+      const [firstCardItemRemoveButton] = await findAllByRemoveButtons();
+
+      await waitFor(() => userEvent.click(firstCardItemRemoveButton));
+
+      expect(getAllByCardHeadings()).toHaveLength(3);
+      expect(getByFirstItemsTitle()).not.toBeInTheDocument();
+    });
+  });
+};
--- src/views/Notes/Notes.tsx
+++ src/views/Notes/Notes.tsx
@@ -1,23 +1,17 @@
-import { connect } from 'react-redux';
-
 import Card from '~/components/molecules/Card/Card';
+import CardList from '~/components/organisms/CardList/CardList';
 import GridTemplate from '~/templates/GridTemplate/GridTemplate';
 
-import type { NoteItem } from '~/commonTypes';
-import type { RootState } from '~/store';
-
-export type NotesProps = {
-  readonly notes: NoteItem[];
-};
-
-const Notes = ({ notes }: NotesProps) => (
+const Notes = () => (
   <GridTemplate>
-    {notes.map(({ id, title, content, created }) => (
-      <Card id={id} title={title} content={content} created={created} key={id} />
-    ))}
+    <CardList variant="notes">
+      {({ data: notes }) =>
+        notes.map(({ id, title, content, created }) => (
+          <Card id={id} title={title} content={content} created={created} key={id} />
+        ))
+      }
+    </CardList>
   </GridTemplate>
 );
 
-const mapStateToProps = ({ items }: RootState) => ({ notes: items.notes });
-
-export default connect(mapStateToProps)(Notes);
+export default Notes;
