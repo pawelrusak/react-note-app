e5f02639732b6167e94563021e8a4d40ad5343c5
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-08-12 23:44:52 +0200

    refactor: create SEL to search items by variant

--- src/store/search/searchSelectors.ts
+++ src/store/search/searchSelectors.ts
@@ -1,8 +1,22 @@
-import { ItemVariants } from '~/commonTypes';
+import { createDraftSafeSelector } from '@reduxjs/toolkit';
 
-import type { RootState } from '~/store';
+import { ItemVariants, Item } from '~/commonTypes';
+import { itemVariantSelector } from '~/store/items/itemsSelectors';
+
+import type { RootState, ItemsState } from '~/store';
 
 export const searchVariantSelector =
   <T extends ItemVariants>(variant: T) =>
   (state: RootState) =>
     state.search[variant];
+
+export const searchItemsByVariantSelector = <T extends ItemVariants>(variant: T) =>
+  createDraftSafeSelector(
+    itemVariantSelector(variant),
+    searchVariantSelector(variant),
+    (items, filter) => {
+      return (items as Item[]).filter(({ title }) =>
+        title.toLowerCase().includes(filter.toLowerCase()),
+      ) as ItemsState[T];
+    },
+  );
