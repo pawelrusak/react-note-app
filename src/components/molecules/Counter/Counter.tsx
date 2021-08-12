ff276da559ab9ab76c745181bf5b6f1a0ff2021e
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-08-13 00:41:05 +0200

    refactor: create <Counter />

---
+++ src/components/molecules/Counter/Counter.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+import Paragraph from '~/components/atoms/Paragraph/Paragraph';
+import Skeleton from '~/components/atoms/Skeleton/Skeleton';
+import { useItemsStatus, usePageTypeContext } from '~/hooks';
+
+const StyledParagraph = styled(Paragraph)`
+  margin: 0;
+  font-weight: ${({ theme }) => theme.bold};
+  font-size: ${({ theme }) => theme.fontSize.m};
+  color: ${({ theme }) => theme.grey500};
+`;
+
+const StyledSkeleton = styled(Skeleton)`
+  height: ${({ theme }) => theme.fontSize.m};
+  width: 10rem;
+`;
+
+const Counter = () => {
+  const pageType = usePageTypeContext();
+  const { isLoading } = useItemsStatus();
+
+  return isLoading() ? <StyledSkeleton /> : <StyledParagraph>6 {pageType}</StyledParagraph>;
+};
+
+export default Counter;
