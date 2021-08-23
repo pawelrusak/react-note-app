9df37851d2ae688e4c78ccd910d803c044c53205
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-08-23 23:55:24 +0200

    feat: add document title to <DetailsPage />

--- src/views/DetailsPage/DetailsPage.tsx
+++ src/views/DetailsPage/DetailsPage.tsx
@@ -1,17 +1,28 @@
-import { useActiveItem } from '~/hooks';
+import pluralize from 'pluralize';
+import { Helmet } from 'react-helmet';
+
+import { useActiveItem, useCurrentPageType } from '~/hooks';
 import DetailsTemplate from '~/templates/DetailsTemplate/DetailsTemplate';
+import { capitalize } from '~/utils';
 
 const DetailsPage = () => {
   const activeItem = useActiveItem();
+  const pageType = useCurrentPageType();
 
   return (
-    <DetailsTemplate
-      title={activeItem.title}
-      created={activeItem.created}
-      content={activeItem.content}
-      articleUrl={activeItem.articleUrl}
-      twitterName={activeItem.twitterName}
-    />
+    <>
+      <Helmet>
+        <title>{`${pluralize.singular(capitalize(pageType))}: "${activeItem.title}"`}</title>
+      </Helmet>
+
+      <DetailsTemplate
+        title={activeItem.title}
+        created={activeItem.created}
+        content={activeItem.content}
+        articleUrl={activeItem.articleUrl}
+        twitterName={activeItem.twitterName}
+      />
+    </>
   );
 };
 
