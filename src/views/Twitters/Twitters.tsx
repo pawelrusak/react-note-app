fd3737ed4dec824c59a7b06fd72696d158076437
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-08-23 21:06:07 +0200

    feat: add document title to <Twitters />

--- src/views/Twitters/Twitters.tsx
+++ src/views/Twitters/Twitters.tsx
@@ -1,24 +1,32 @@
+import { Helmet } from 'react-helmet';
+
 import Card from '~/components/molecules/Card/Card';
 import CardList from '~/components/organisms/CardList/CardList';
 import GridTemplate from '~/templates/GridTemplate/GridTemplate';
 
 const Twitters = () => (
-  <GridTemplate>
-    <CardList variant="twitters">
-      {({ data: twitters }) =>
-        twitters.map(({ id, title, content, twitterName, created }) => (
-          <Card
-            id={id}
-            title={title}
-            content={content}
-            twitterName={twitterName}
-            created={created}
-            key={id}
-          />
-        ))
-      }
-    </CardList>
-  </GridTemplate>
+  <>
+    <Helmet>
+      <title>Twitters</title>
+    </Helmet>
+
+    <GridTemplate>
+      <CardList variant="twitters">
+        {({ data: twitters }) =>
+          twitters.map(({ id, title, content, twitterName, created }) => (
+            <Card
+              id={id}
+              title={title}
+              content={content}
+              twitterName={twitterName}
+              created={created}
+              key={id}
+            />
+          ))
+        }
+      </CardList>
+    </GridTemplate>
+  </>
 );
 
 export default Twitters;
