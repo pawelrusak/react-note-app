cca52f1ce90061adea2de901477fdd18080fd6cd
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-08-23 21:05:05 +0200

    feat: add document title to <Notes />

--- src/views/Notes/Notes.tsx
+++ src/views/Notes/Notes.tsx
@@ -1,17 +1,25 @@
+import { Helmet } from 'react-helmet';
+
 import Card from '~/components/molecules/Card/Card';
 import CardList from '~/components/organisms/CardList/CardList';
 import GridTemplate from '~/templates/GridTemplate/GridTemplate';
 
 const Notes = () => (
-  <GridTemplate>
-    <CardList variant="notes">
-      {({ data: notes }) =>
-        notes.map(({ id, title, content, created }) => (
-          <Card id={id} title={title} content={content} created={created} key={id} />
-        ))
-      }
-    </CardList>
-  </GridTemplate>
+  <>
+    <Helmet>
+      <title>Notes</title>
+    </Helmet>
+
+    <GridTemplate>
+      <CardList variant="notes">
+        {({ data: notes }) =>
+          notes.map(({ id, title, content, created }) => (
+            <Card id={id} title={title} content={content} created={created} key={id} />
+          ))
+        }
+      </CardList>
+    </GridTemplate>
+  </>
 );
 
 export default Notes;
