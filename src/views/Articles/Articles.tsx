77d6e45b5bd0603d57c0dafa2b6212e7306a4886
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-08-23 21:03:07 +0200

    feat: add document title to <Articles />

--- src/views/Articles/Articles.tsx
+++ src/views/Articles/Articles.tsx
@@ -1,24 +1,31 @@
+import { Helmet } from 'react-helmet';
+
 import Card from '~/components/molecules/Card/Card';
 import CardList from '~/components/organisms/CardList/CardList';
 import GridTemplate from '~/templates/GridTemplate/GridTemplate';
 
 const Articles = () => (
-  <GridTemplate>
-    <CardList variant="articles">
-      {({ data: articles }) =>
-        articles.map(({ id, title, content, articleUrl, created }) => (
-          <Card
-            id={id}
-            title={title}
-            content={content}
-            articleUrl={articleUrl}
-            created={created}
-            key={id}
-          />
-        ))
-      }
-    </CardList>
-  </GridTemplate>
+  <>
+    <Helmet>
+      <title>Articles</title>
+    </Helmet>
+    <GridTemplate>
+      <CardList variant="articles">
+        {({ data: articles }) =>
+          articles.map(({ id, title, content, articleUrl, created }) => (
+            <Card
+              id={id}
+              title={title}
+              content={content}
+              articleUrl={articleUrl}
+              created={created}
+              key={id}
+            />
+          ))
+        }
+      </CardList>
+    </GridTemplate>
+  </>
 );
 
 export default Articles;
