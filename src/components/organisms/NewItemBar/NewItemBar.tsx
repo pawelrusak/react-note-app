c1643c3e52fa5fd81201adde67a0c0f21cfab28f
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-08-26 01:55:13 +0200

    refactor: simplify <NewItemBar /> by use utils

--- src/components/organisms/NewItemBar/NewItemBar.tsx
+++ src/components/organisms/NewItemBar/NewItemBar.tsx
@@ -5,11 +5,10 @@ import Button from '~/components/atoms/Button/Button';
 import Heading from '~/components/atoms/Heading/Heading';
 import Input from '~/components/atoms/Input/Input';
 import Field from '~/components/molecules/Field/Field';
-import { NEW_ITEM_VARIANTS_KEYS } from '~/constants/data';
 import { TEST_ID } from '~/constants/tests';
 import { usePageTypeContext, useAddItemAction } from '~/hooks';
 import * as styledMixin from '~/theme/mixins';
-import { hasPropertiesWithTrueValues } from '~/utils';
+import { isNewItemVariantTouched } from '~/utils';
 import { newItemSchema } from '~/validations';
 
 import type { ActiveColorArgs } from '~/theme/mixins';
@@ -99,11 +98,7 @@ const NewItemBar = ({ isVisible, handleClose }: NewItemBarProps) => {
             <Button
               type="submit"
               pending={isSubmitting}
-              disabled={
-                isSubmitting ||
-                (hasPropertiesWithTrueValues(touched, [...NEW_ITEM_VARIANTS_KEYS[pageContext]]) &&
-                  !isValid)
-              }
+              disabled={isSubmitting || (isNewItemVariantTouched(touched, pageContext) && !isValid)}
               activecolor={pageContext}
             >
               Add Note
