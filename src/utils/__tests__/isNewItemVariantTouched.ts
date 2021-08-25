dbfb0d657f526cc3000a2fc20a3a6f9845bdf704
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-08-26 01:53:18 +0200

    test: add tests for isNewItemVariantTouched util

---
+++ src/utils/__tests__/isNewItemVariantTouched.ts
@@ -0,0 +1,69 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { isNewItemVariantTouched } from '../index';
+
+import type { FormikTouched } from 'formik';
+import type { NewItem, ItemVariants } from '~/commonTypes';
+
+type NewItemFormikTouched = FormikTouched<NewItem>;
+
+const touchedNewItem: NewItemFormikTouched = {
+  title: true,
+  content: true,
+  twitterName: true,
+  articleUrl: true,
+};
+
+const notTouchedNewItem: NewItemFormikTouched = {
+  title: false,
+  content: false,
+  twitterName: false,
+  articleUrl: false,
+};
+
+const NEW_ITEM_VARIANTS: ItemVariants[] = ['notes', 'articles', 'twitters'];
+
+describe('isNewItemVariantTouched utils', () => {
+  it.each(NEW_ITEM_VARIANTS)(
+    'if all properties is true then for the new %s item should return true',
+    (variant) => {
+      expect(isNewItemVariantTouched(touchedNewItem, variant)).toBeTrue();
+    },
+  );
+
+  it.each(NEW_ITEM_VARIANTS)(
+    "if all properties is false then for the new %s item should return false'",
+    (variant) => {
+      expect(isNewItemVariantTouched(notTouchedNewItem, variant)).toBeFalse();
+    },
+  );
+
+  it.each(NEW_ITEM_VARIANTS)(
+    "if title property is not defined then for the new %s item should return false'",
+    (variant) => {
+      const { title, ...touchedNewItemWithoutTitle } = touchedNewItem;
+
+      expect(isNewItemVariantTouched(touchedNewItemWithoutTitle, variant)).toBeFalse();
+    },
+  );
+
+  it.each(NEW_ITEM_VARIANTS)(
+    "if content property is not defined then for the new %s item should return false'",
+    (variant) => {
+      const { content, ...touchedNewItemWithoutContent } = touchedNewItem;
+
+      expect(isNewItemVariantTouched(touchedNewItemWithoutContent, variant)).toBeFalse();
+    },
+  );
+
+  it("if twitterName property is not defined then for new twitter item should return false'", () => {
+    const { twitterName, ...touchedNewItemWithoutTwitterName } = touchedNewItem;
+
+    expect(isNewItemVariantTouched(touchedNewItemWithoutTwitterName, 'twitters')).toBeFalse();
+  });
+
+  it("if the articleUrl property is not defined then for new article item should return false'", () => {
+    const { articleUrl, ...touchedNewItemWithoutArticleUrl } = touchedNewItem;
+
+    expect(isNewItemVariantTouched(touchedNewItemWithoutArticleUrl, 'articles')).toBeFalse();
+  });
+});
