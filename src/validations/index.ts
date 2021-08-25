a30d9fe5ce43de20fbfcd0d2c61236942f7fb5e1
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-08-25 17:22:45 +0200

    refactor: create the new item validation schema

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -1,6 +1,44 @@
 import * as yup from 'yup';
 
+import type { SchemaOf } from 'yup';
+import type { NewItem, ItemVariants } from '~/commonTypes';
+
 export const authSchema = yup.object().shape({
   email: yup.string().email().required(),
   password: yup.string().min(6).max(50).required(),
+});
+
+type NewItemSchema = SchemaOf<NewItem & { variant: ItemVariants }>;
+
+export const newItemSchema: NewItemSchema = yup.object().shape({
+  title: yup.string().max(50).required(),
+  content: yup.string().max(5000).required(),
+  variant: yup.mixed().oneOf<ItemVariants>(['notes', 'twitters', 'articles']).defined(),
+  /**
+   * {@link https://help.twitter.com/en/managing-your-account/twitter-username-rules Twitter username rules}
+   */
+  twitterName: yup
+    .string()
+    .optional()
+    .when('variant', {
+      // eslint-disable-next-line
+      // @ts-expect-error
+      is: (variant) => variant === 'twitters',
+      then: yup
+        .string()
+
+        .min(4)
+        .max(15)
+        .matches(/^[a-zA-Z0-9_]*$/)
+        .required(),
+    }),
+  articleUrl: yup
+    .string()
+    .optional()
+    .when('variant', {
+      // eslint-disable-next-line
+      // @ts-expect-error
+      is: (variant) => variant === 'articles',
+      then: yup.string().url().required(),
+    }),
 });
