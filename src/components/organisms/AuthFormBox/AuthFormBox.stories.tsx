9f6288c5ff6fc7eb18e820cfcf9146e6e1a36f52
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-08-26 20:29:48 +0200

    chore: add form validation to AuthFormBox stories

--- src/components/organisms/AuthFormBox/AuthFormBox.stories.tsx
+++ src/components/organisms/AuthFormBox/AuthFormBox.stories.tsx
@@ -2,6 +2,7 @@ import withFormik from 'storybook-formik';
 import StoryRouter from 'storybook-react-router';
 
 import AuthFormBox, { AuthFormBoxProps } from './AuthFormBox';
+import { authSchema } from '~/validations';
 import { AuthFormBoxCardDecorator } from '~~/.storybook/decorators';
 
 import type { Meta, Story } from '@storybook/react';
@@ -18,8 +19,28 @@ export const Login = Template.bind({});
 Login.args = {
   formVariant: 'login',
 };
+Login.parameters = {
+  formik: {
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: authSchema,
+    onSubmit: () => null,
+  },
+};
 
 export const Register = Template.bind({});
 Register.args = {
   formVariant: 'register',
+};
+Register.parameters = {
+  formik: {
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: authSchema,
+    onSubmit: () => null,
+  },
 };
