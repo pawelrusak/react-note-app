011c872218537ba921618474b5e4d51d84fe7e6e
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-08-26 23:28:29 +0200

    refactor: use <AuthFormBox /> in <RegisterPage />

--- src/views/RegisterPage/RegisterPage.tsx
+++ src/views/RegisterPage/RegisterPage.tsx
@@ -1,11 +1,7 @@
-import { Formik, Form } from 'formik';
-import { Link, Redirect } from 'react-router-dom';
-import styled from 'styled-components';
+import { Formik } from 'formik';
+import { Redirect } from 'react-router-dom';
 
-import Button from '~/components/atoms/Button/Button';
-import Heading from '~/components/atoms/Heading/Heading';
-import Input from '~/components/atoms/Input/Input';
-import Field from '~/components/molecules/Field/Field';
+import AuthFormBox from '~/components/organisms/AuthFormBox/AuthFormBox';
 import { AUTH_ERRORS_CODES } from '~/constants/auth';
 import { useAuth } from '~/hooks';
 import { routes } from '~/routes';
@@ -15,34 +11,12 @@ import * as validation from '~/validations';
 
 import type firebase from 'firebase';
 
-const StyledForm = styled(Form)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
-
-const StyledInput = styled(Input)`
-  height: 40px;
-  width: 100%;
-`;
-
-const StyledField = styled(Field)`
-  display: block;
-  width: 300px;
-`;
-
-const StyledLink = styled(Link)`
-  display: block;
-  font-weight: ${({ theme }) => theme.bold};
-  font-size: ${({ theme }) => theme.fontSize.xs};
-  color: black;
-  text-transform: uppercase;
-  margin: 20px 0 50px;
-`;
-
 const RegisterPage = () => {
   const { register, userID } = useAuth();
+
+  if (userID) {
+    return <Redirect to={routes.home} />;
+  }
 
   return (
     <>
@@ -67,40 +41,7 @@ const RegisterPage = () => {
             }
           }}
         >
-          {({ isSubmitting, touched, isValid }) => {
-            if (userID) {
-              return <Redirect to={routes.home} />;
-            }
-            return (
-              <>
-                <Heading id="register-page-form">Sign up</Heading>
-                <StyledForm aria-labelledby="register-page-form">
-                  <StyledField
-                    name="email"
-                    type="email"
-                    placeholder="Login"
-                    component={StyledInput}
-                    aria-required="true"
-                  />
-                  <StyledField
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    component={StyledInput}
-                    aria-required="true"
-                  />
-                  <Button
-                    type="submit"
-                    pending={isSubmitting}
-                    disabled={isSubmitting || ((touched.email || touched.password) && !isValid)}
-                  >
-                    register
-                  </Button>
-                </StyledForm>
-                <StyledLink to={routes.login}>I want to log in!</StyledLink>
-              </>
-            );
-          }}
+          <AuthFormBox formVariant="register" />
         </Formik>
       </AuthTemplate>
     </>
