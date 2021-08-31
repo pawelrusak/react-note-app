214540ea5b6f8032210be839a196d5ade15b6a59
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-08-31 21:35:34 +0200

    refactor: remove unused SC mixins

--- src/theme/mixins.ts
+++ src/theme/mixins.ts
@@ -3,25 +3,6 @@ import { css } from 'styled-components';
 
 import type { ItemVariants } from '~/commonTypes';
 
-export type ActiveColorArgs = { readonly activecolor?: ItemVariants };
-
-export const activecolor = css<ActiveColorArgs>`
-  background-color: ${(props) =>
-    props.activecolor ? props.theme[props.activecolor] : props.theme.notes};
-`;
-
-export type LightenActiveColorArgs = ActiveColorArgs;
-
-export const lightenActiveColor = css<LightenActiveColorArgs>`
-  background-color: ${(props) =>
-    polished.lighten(0.1, props.activecolor ? props.theme[props.activecolor] : props.theme.notes)};
-`;
-
-export const darkenActiveColor = css<LightenActiveColorArgs>`
-  background-color: ${(props) =>
-    polished.darken(0.075, props.activecolor ? props.theme[props.activecolor] : props.theme.notes)};
-`;
-
 export const lightenBlack = css`
   ${({ theme }) => polished.lighten(0.3, theme.black)}
 `;
