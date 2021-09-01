c2c229a6c499478d918fa38b969d0ff852ad40e3
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-09-01 18:19:37 +0200

    refactor: remove unnecessary CurrentPageType Hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,7 +2,6 @@ export * from 'react-use';
 export { useItemsStatus } from './custom/useItemsStatus';
 export { useHistoryPush } from './custom/useHistoryPush';
 export { useRemoveItemAction } from './custom/useRemoveItemAction';
-export { useCurrentPageType } from './custom/useCurrentPageType';
 export { useCurrentPageVariant } from '~/context';
 export { useFetchItems } from './custom/useFetchItems';
 export { useActiveItem } from './custom/useActiveItem';
