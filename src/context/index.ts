50f3d91fb62f29f050ac2b44a0829c0082a130e2
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-09-01 22:08:17 +0200

    refactor: remove the PageContext

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,9 +1,1 @@
-import React from 'react';
-
-import type { ItemVariants } from '~/commonTypes';
-
-const PageContext = React.createContext<ItemVariants | undefined>('notes');
-
-export default PageContext;
-
 export { useCurrentPageVariant, CurrentPageVariantProvider } from './currentPageVariant';
