0ae7a2e06b206d2c5dc304e80888d2775c16af18
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-09-01 12:57:07 +0200

    refactor: reexport the CurrentPageVariant Context

--- src/context/index.ts
+++ src/context/index.ts
@@ -5,3 +5,5 @@ import type { ItemVariants } from '~/commonTypes';
 const PageContext = React.createContext<ItemVariants | undefined>('notes');
 
 export default PageContext;
+
+export { useCurrentPageVariant, CurrentPageVariantProvider } from './currentPageVariant';
