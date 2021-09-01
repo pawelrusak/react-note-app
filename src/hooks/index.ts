b2eef5c279ddf926f206a7dacd1af4639f3a331b
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-09-01 12:59:05 +0200

    refactor: reexport the Current Page Variant Hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -4,6 +4,7 @@ export { usePageTypeContext } from './custom/usePageTypeContext';
 export { useHistoryPush } from './custom/useHistoryPush';
 export { useRemoveItemAction } from './custom/useRemoveItemAction';
 export { useCurrentPageType } from './custom/useCurrentPageType';
+export { useCurrentPageVariant } from '~/context';
 export { useFetchItems } from './custom/useFetchItems';
 export { useActiveItem } from './custom/useActiveItem';
 export { useAuth } from './custom/useAuth';
