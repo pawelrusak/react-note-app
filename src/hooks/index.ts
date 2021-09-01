6545d5eab0f707c14f5317a3b8b3d23764639d8d
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-09-01 17:34:07 +0200

    refactor: remove unused Page Type Context Hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,5 @@
 export * from 'react-use';
 export { useItemsStatus } from './custom/useItemsStatus';
-export { usePageTypeContext } from './custom/usePageTypeContext';
 export { useHistoryPush } from './custom/useHistoryPush';
 export { useRemoveItemAction } from './custom/useRemoveItemAction';
 export { useCurrentPageType } from './custom/useCurrentPageType';
