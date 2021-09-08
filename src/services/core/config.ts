a0a017272c9f8dec814a00a534588f9c6c20e084
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-09-08 17:24:29 +0200

    refactor: rename environment variables

--- src/services/core/config.ts
+++ src/services/core/config.ts
@@ -4,13 +4,13 @@
  * @see {@link https://firebase.google.com/docs/projects/learn-more#config-files-objects}
  */
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_DEVELOPMENT_API_KEY,
-  authDomain: `${process.env.REACT_APP_FIREBASE_DEVELOPMENT_PROJECT_ID}.firebaseapp.com`,
-  databaseURL: `https://${process.env.REACT_APP_FIREBASE_DEVELOPMENT_PROJECT_ID}.firebaseio.com`,
-  projectId: `${process.env.REACT_APP_FIREBASE_DEVELOPMENT_PROJECT_ID}`,
-  storageBucket: `${process.env.REACT_APP_FIREBASE_DEVELOPMENT_PROJECT_ID}.appspot.com`,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_DEVELOPMENT_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_DEVELOPMENT_APP_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  databaseURL: `https://${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseio.com`,
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export default config;
