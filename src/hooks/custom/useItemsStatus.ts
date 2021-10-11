69bf2d4b19c9c30fa6bf491a68be06468180a4f6
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-10-11 22:50:39 +0200

    refactor: use enum status in the Item Status Hook

--- src/hooks/custom/useItemsStatus.ts
+++ src/hooks/custom/useItemsStatus.ts
@@ -3,7 +3,11 @@ import { useSelector } from 'react-redux';
 import type { RootState } from '~/store';
 
 export const useItemsStatus = () => {
-  const loading = useSelector((state: RootState) => state.items.isLoading);
+  const status = useSelector((state: RootState) => state.items.status);
 
-  return { isLoading: () => loading };
+  return {
+    isLoading: () => status === 'idle' || status === 'loading',
+    isSucceeded: () => status === 'succeeded',
+    isFailed: () => status === 'failed',
+  };
 };
