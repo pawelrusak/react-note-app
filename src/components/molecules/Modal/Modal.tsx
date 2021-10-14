59e4f68e300bb6bf61213a47387d644f93307a17
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-10-14 18:02:29 +0200

    refactor: simplify <Modal> by using <TextButton>

--- src/components/molecules/Modal/Modal.tsx
+++ src/components/molecules/Modal/Modal.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import Button from '~/components/atoms/Button/Button';
 import Heading from '~/components/atoms/Heading/Heading';
 import Paragraph from '~/components/atoms/Paragraph/Paragraph';
+import TextButton from '~/components/atoms/TextButton/TextButton';
 import { useClickAway } from '~/hooks';
 import * as styledMixin from '~/theme/mixins';
 import { Portal } from '~/utils/components';
@@ -77,54 +78,6 @@ const StyledFooter = styled.div`
   }
 `;
 
-const StyledSecondaryButton = styled.button`
-  color: #434343;
-  font-weight: ${({ theme }) => theme.bold};
-  font-size: 1.5rem;
-  text-transform: lowercase;
-  margin: 0;
-  border: none;
-  background: transparent;
-  cursor: pointer;
-  line-height: 1;
-  position: relative;
-  margin: 0;
-  padding: 0;
-  text-shadow: white 2px 0 0, white -2px 0 0, white 0 1px 0;
-
-  &::after,
-  &::before {
-    position: absolute;
-    content: '';
-    bottom: 0;
-    left: 0;
-    height: 1px;
-    background-color: currentColor;
-  }
-
-  &::after {
-    width: 1ch;
-  }
-
-  &::before {
-    width: 100%;
-    transform: scaleX(0);
-    // behind the text-shadow
-    z-index: -1;
-  }
-
-  @media (prefers-reduced-motion: no-preference) {
-    &::before {
-      transition: transform 0.15s ease-in-out;
-      transform-origin: left;
-    }
-  }
-
-  &:hover::before {
-    transform: scaleX(100%);
-  }
-`;
-
 const StyledDescription = styled(Paragraph)`
   margin: 0;
   font-size: 1.5rem;
@@ -166,6 +119,6 @@ Modal.Body = StyledBody;
 Modal.Description = StyledDescription;
 Modal.Footer = StyledFooter;
 Modal.PrimaryButton = StyledPrimaryButton;
-Modal.SecondaryButton = StyledSecondaryButton;
+Modal.SecondaryButton = TextButton;
 
 export default Modal;
