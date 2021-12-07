91f5ad70ed2dc5cf1c07cfa1d22f57aaf04f3446
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2021-12-07 21:59:31 +0100

    feat: add states to <DetailsPage/>

--- src/views/DetailsPage/DetailsPage.tsx
+++ src/views/DetailsPage/DetailsPage.tsx
@@ -1,27 +1,54 @@
 import pluralize from 'pluralize';
+import styled from 'styled-components';
 
-import { useActiveItem, useCurrentPageVariant } from '~/hooks';
+import Heading from '~/components/atoms/Heading/Heading';
+import { useCurrentPageVariant, useDetailsItem } from '~/hooks';
 import DetailsTemplate from '~/templates/DetailsTemplate/DetailsTemplate';
+import SkeletonDetailsTemplate from '~/templates/SkeletonDetailsTemplate/SkeletonDetailsTemplate';
+import UserPageTemplate from '~/templates/UserPageTemplate/UserPageTemplate';
 import { DocumentTitle } from '~/utils/components';
 
+const StyledEmptyStateWrapper = styled.div`
+  display: grid;
+  place-items: center;
+  min-height: 100vh;
+`;
+
+const StyledEmptyStateTitle = styled(Heading)`
+  color: ${({ theme }) => theme.grey500};
+`;
+
 const DetailsPage = () => {
-  const activeItem = useActiveItem();
   const pageVariant = useCurrentPageVariant();
+  const { data: detailsItem, isSucceeded, isLoading, isFailed } = useDetailsItem();
 
   return (
     <>
       <DocumentTitle capitalize>
-        {pluralize.singular(pageVariant)}: &quot;{activeItem.title}&quot;
+        {pluralize.singular(pageVariant)}: &quot;
+        {isSucceeded() && detailsItem ? detailsItem.title : 'Loading...'}&quot;
       </DocumentTitle>
 
-      <DetailsTemplate
-        id={activeItem.id}
-        title={activeItem.title}
-        created={activeItem.created}
-        content={activeItem.content}
-        articleUrl={activeItem.articleUrl}
-        twitterName={activeItem.twitterName}
-      />
+      {isLoading() && <SkeletonDetailsTemplate />}
+      {isFailed() && (
+        <UserPageTemplate>
+          <StyledEmptyStateWrapper>
+            <StyledEmptyStateTitle as="h2">
+              No {pluralize.singular(pageVariant)} found.
+            </StyledEmptyStateTitle>
+          </StyledEmptyStateWrapper>
+        </UserPageTemplate>
+      )}
+      {isSucceeded() && detailsItem && (
+        <DetailsTemplate
+          id={detailsItem.id}
+          title={detailsItem.title}
+          created={detailsItem.created}
+          content={detailsItem.content}
+          articleUrl={detailsItem.articleUrl}
+          twitterName={detailsItem.twitterName}
+        />
+      )}
     </>
   );
 };
