6aaa622ca6a99300d12090676b4237115dc645ca
Paweł Rusak <78496242+pawelrusak@users.noreply.github.com>
2022-02-02 23:35:34 +0100

    refactor: create <Search /> (#151)
    * refactor: create <Search />
    
    * chore: create stories for <Search />
    
    * refactor: use <Search /> in <CardListTemplate />

---
+++ src/components/molecules/Search/Search.tsx
@@ -0,0 +1,30 @@
+import Input from '~/components/atoms/Input/Input';
+import { useCurrentPageVariant, useSearchState } from '~/hooks';
+import { VisuallyHidden } from '~/utils';
+
+const SearchForm = () => {
+  const pageVariant = useCurrentPageVariant();
+  const [search, setSearch] = useSearchState(pageVariant);
+
+  return (
+    <div role="search">
+      <form role="search">
+        <p>
+          <VisuallyHidden as="label" htmlFor="search-input">
+            Search
+          </VisuallyHidden>
+          <Input
+            id="search-input"
+            type="search"
+            search
+            placeholder="Search"
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+          />
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default SearchForm;
