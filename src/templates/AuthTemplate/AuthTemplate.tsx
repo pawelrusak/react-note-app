b23a5514416938a4dec09dfc2ea32c3ab7319528
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2022-06-02 00:28:42 +0200

    feat: create responsive styles for AuthTemplate

--- src/templates/AuthTemplate/AuthTemplate.tsx
+++ src/templates/AuthTemplate/AuthTemplate.tsx
@@ -1,17 +1,32 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
+import bulbIcon from '~/assets/icons/bulb.svg';
 import logoImg from '~/assets/icons/logo.svg';
-import Heading from '~/components/atoms/Heading/Heading';
+import penIcon from '~/assets/icons/pen.svg';
+import twitterIcon from '~/assets/icons/twitter.svg';
+
+import ButtonIcon from '~/components/atoms/ButtonIcon/ButtonIcon';
+import Paragraph from '~/components/atoms/Paragraph/Paragraph';
+import { media } from '~/theme/mixins';
 import { VisuallyHidden } from '~/utils';
+
+export const styledWrapperXsStyles = css`
+  background-color: ${({ theme }) => theme.notes};
+`;
 
 const StyledWrapper = styled.div`
   width: 100%;
-  height: 100vh;
-  background-color: ${({ theme }) => theme.notes};
+  min-height: 100vh;
+  background-color: white;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  padding: 0 3rem;
+
+  ${media.greaterThan('xs')`
+     ${styledWrapperXsStyles};
+  `}
 `;
 
 const StyledHeader = styled.header`
@@ -19,20 +34,95 @@ const StyledHeader = styled.header`
 `;
 
 const StyledLogo = styled.img`
-  width: 200px;
+  width: 14.1rem;
   height: auto;
+  margin-bottom: 6.6rem;
+
+  ${media.greaterThan('xs')`
+    margin-bottom: 1.1rem; 
+    filter: invert(1);
+  `}
+
+  ${media.greaterThan('md')`
+    width: 17rem; 
+    
+  `}
+
+  ${media.greaterThan('lg')`
+    margin-top: ${(45 / 1080) * 100}vh;
+  `}  
+
+  ${media.greaterThan('xl')`
+    margin-bottom: calc(1.1rem + ${(26 / 1080) * 100}vh);
+    width: 23.8rem; 
+  `}
 `;
 
 const StyledAuthCard = styled.div`
-  width: 400px;
-  height: 400px;
+  width: 100%;
+
   background-color: white;
-  border-radius: 10px;
-  box-shadow: 0 10px 20px -10px rgba(0, 0, 0, 0.2);
-  display: flex;
-  /* flex-direction: column; */
-  align-items: center;
-  justify-content: center;
+  border-radius: 7px;
+
+  ${media.greaterThan('xs')`
+    width: 42rem;
+    min-height: 42rem;
+    margin-bottom: 0.8rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 5rem;
+    box-shadow: 0px 3px 8.5px rgba(0, 0, 0, 0.16);
+  `}
+
+  ${media.greaterThan('xl')`
+    width: 57.4rem;
+    min-height: 51.6rem;
+    padding: 5.8rem 10rem;
+  `}
+`;
+
+const StyledSubHeading = styled(Paragraph)`
+  display: none;
+  font-size: 2.6rem;
+  line-height: 3rem;
+
+  font-weight: ${({ theme }) => theme.bold};
+
+  ${media.greaterThan('xs')`
+    display: block;
+    margin-bottom: 1.1rem; 
+  `}
+
+  ${media.greaterThan('md')`
+    font-size: 3.2rem;
+    line-height: 3.6rem;
+  `}
+
+  ${media.greaterThan('xl')`
+    font-size: 3.8rem;
+    line-height: 4rem;
+  `}
+`;
+
+const StyledTextBlock = styled.span`
+  display: block;
+`;
+
+const ButtonIconGroup = styled.div`
+  display: none;
+
+  ${media.greaterThan('xl')`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    
+    width: 26.9rem;
+    margin: 0 auto;
+    margin-top: calc(1.1rem + ${(14 / 1080) * 100}vh);
+    margin-bottom: calc(1.1rem + ${(20 / 1080) * 100}vh);
+  `}
 `;
 
 export type AuthTemplateProps = {
@@ -46,7 +136,14 @@ const AuthTemplate = ({ children }: AuthTemplateProps) => (
         <StyledLogo src={logoImg} aria-hidden alt="FavNote." />
         <VisuallyHidden>FavNote.</VisuallyHidden>
       </h1>
-      <Heading as="p">Your new favorite online notes experience</Heading>
+      <StyledSubHeading>
+        <StyledTextBlock>Your new favorite</StyledTextBlock> online notes experience
+      </StyledSubHeading>
+      <ButtonIconGroup aria-hidden>
+        <ButtonIcon icon={penIcon} />
+        <ButtonIcon icon={twitterIcon} />
+        <ButtonIcon icon={bulbIcon} />
+      </ButtonIconGroup>
     </StyledHeader>
     <StyledAuthCard>{children}</StyledAuthCard>
   </StyledWrapper>
