385b22f76b1e6187d61c17d3f5bde9310755d468
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2022-06-02 21:36:31 +0200

    feat: create the responsive styles for <Card/>

--- src/components/molecules/Card/Card.tsx
+++ src/components/molecules/Card/Card.tsx
@@ -1,6 +1,6 @@
 import { darken } from 'polished';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import LinkIcon from '~/assets/icons/link.svg';
 import Button from '~/components/atoms/Button/Button';
@@ -10,19 +10,32 @@ import Time from '~/components/atoms/Time/Time';
 import ConfirmationModal from '~/components/molecules/ConfirmationModal/ConfirmationModal';
 import { TEST_ID } from '~/constants';
 import { useHistoryPush, useCurrentPageVariant, useConfirmationModal } from '~/hooks';
+import { media } from '~/theme/mixins';
 import * as styledMixin from '~/theme/mixins';
 
 import type { Variants, Item } from '~/commonTypes';
 import type { VariantColorValueProp } from '~/theme/mixins';
 
+const cardPadding = css`
+  padding: 1rem 1.9rem;
+
+  ${media.greaterThan('xs')`
+    padding: 1.7rem 3.2rem;
+  `}
+`;
+
 const StyledWrapper = styled.div`
-  min-height: 380px;
-  box-shadow: 0 10px 30px -10px hsla(0, 0%, 0%, 0.1);
-  border-radius: 10px;
+  min-height: 30.7rem;
+  box-shadow: 0 7px 7px hsla(0, 0%, 0%, 0.09);
+  border-radius: 0.7rem;
   overflow: hidden;
   position: relative;
   display: grid;
-  grid-template-rows: 0.25fr 1fr;
+  grid-template-rows: auto 1fr;
+
+  ${media.greaterThan('xs')`
+    min-height: 38.4rem;
+  `}
 `;
 
 // create new component because the base one is reuse in others places
@@ -31,20 +44,25 @@ const StyledWrapperWithHover = styled(StyledWrapper)`
     transition: transform 0.15s ease-out;
 
     &:hover {
-      transform: scale(1.015);
+      transform: scale(1.05);
     }
   }
 `;
 
 const HeaderWrapper = styled.header<VariantColorValueProp>`
   position: relative;
-  padding: 17px 30px;
   background-color: ${styledMixin.variantColorValue()};
   ${styledMixin.zIndexDeclaration('cardHeader')};
+
+  ${cardPadding}
 `;
 
+type HeaderWrapperWithHoverProp = {
+  variantExtraPadding?: Variants;
+} & VariantColorValueProp;
+
 // create new component because the base one is reuse in others places
-const HeaderWrapperWithHover = styled(HeaderWrapper)<VariantColorValueProp>`
+const HeaderWrapperWithHover = styled(HeaderWrapper)<HeaderWrapperWithHoverProp>`
   cursor: pointer;
 
   @media (prefers-reduced-motion: no-preference) {
@@ -55,33 +73,62 @@ const HeaderWrapperWithHover = styled(HeaderWrapper)<VariantColorValueProp>`
   &:hover {
     background-color: ${styledMixin.variantColorValue({ darken: true })};
   }
+
+  ${({ variantExtraPadding }) =>
+    variantExtraPadding === 'twitters' &&
+    css`
+      padding-right: 12.8rem;
+    `}
+
+  ${({ variantExtraPadding }) =>
+    variantExtraPadding === 'articles' &&
+    css`
+      padding-right: 8.7rem;
+    `}
 `;
 
 const ContentWrapper = styled.div`
-  padding: 17px 30px;
   background-color: white;
   display: flex;
   flex-direction: column;
+  align-items: flex-start;
+
+  ${cardPadding}
+
+  padding-top: 1.5rem;
+  padding-bottom: 1.3rem;
+
+  ${media.greaterThan('sm')`
+    padding-top: 2.9rem;
+    padding-bottom: 2.4rem;
+  `}
 `;
 
 const DateInfo = styled(Time)`
-  margin: 0 0 5px;
   font-weight: ${({ theme }) => theme.bold};
   font-size: ${({ theme }) => theme.fontSize.xs};
+  letter-spacing: 0.04em;
 `;
 
 const StyledHeading = styled(Heading)`
-  margin: 5px 0 0;
+  margin-bottom: 0.5rem;
+
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
 `;
 
 const StyledAvatar = styled.img`
-  width: 86px;
-  height: 86px;
-  border: 5px solid ${({ theme }) => theme.twitters};
-  border-radius: 50px;
+  width: 10rem;
+  height: 10rem;
+  border: 0.6rem solid ${({ theme }) => theme.twitters};
+  border-radius: 5rem;
+
   position: absolute;
-  right: 25px;
-  top: 25px;
+  right: 1.4rem;
+  top: 1rem;
+
   @media (prefers-reduced-motion: no-preference) {
     will-change: border-color;
     transition: border-color 0.2s ease-in-out;
@@ -94,35 +141,25 @@ const StyledAvatar = styled.img`
 
 const StyledArticleLink = styled.a`
   display: block;
-  width: 47px;
-  height: 47px;
-  border-radius: 50px;
+  width: 4.7rem;
+  height: 4.7rem;
+  border-radius: 5rem;
   background: white url(${LinkIcon}) no-repeat;
   background-size: 60%;
   background-position: 50%;
   position: absolute;
-  right: 25px;
+  right: 2rem;
   top: 50%;
   transform: translateY(-50%);
 `;
 
-/**
- * @todo Should I add a fallback for IE?
- */
 const StyledParagraph = styled(Paragraph)`
   display: -webkit-box;
   -webkit-line-clamp: 4;
   -webkit-box-orient: vertical;
   overflow: hidden;
-  margin: 0.5rem 0 0;
-  line-height: ${({ theme }) => theme.lineHeight};
-
-  @supports not (-webkit-line-clamp: 4) {
-    display: initial;
-    /* theme-paragraph-font-size x 4 [line clamp] x 1.2[standard default browsers line height] */
-    height: ${({ theme }) => `${parseFloat(theme.fontSize.s) * 4 * 1.2}rem`};
-    overflow-y: hidden;
-  }
+  margin-bottom: 0.6rem;
+  line-height: 2.7rem;
 `;
 
 const StyledDetailsLink = styled(Link)`
@@ -132,7 +169,10 @@ const StyledDetailsLink = styled(Link)`
   color: ${({ theme }) => theme.black};
   margin-bottom: auto;
   font-variant-caps: all-small-caps;
-  line-height: ${({ theme }) => theme.lineHeight};
+  font-size: 1.5rem;
+  line-height: 1;
+  letter-spacing: -0.06rem;
+
   position: relative;
   align-self: flex-start;
 
@@ -141,7 +181,7 @@ const StyledDetailsLink = styled(Link)`
     position: absolute;
     left: 0;
     // ((line height * font-height) - font-height) / half-white-space-around-font
-    bottom: ${({ theme }) => `calc(${(+theme.lineHeight * 1.6 - 1.6) / 2}rem - 0.2rem)`};
+    bottom: -0.15rem;
     height: 0.2rem;
     width: 100%;
     background-color: currentColor;
@@ -186,8 +226,9 @@ const Card = <V extends Variants>({
         data-testid={TEST_ID.CARD.HEADER}
         onClick={historyPush}
         variant={pageVariant}
+        variantExtraPadding={pageVariant}
       >
-        <StyledHeading data-testid={TEST_ID.CARD.TITLE} as="h3">
+        <StyledHeading data-testid={TEST_ID.CARD.TITLE} title={title} as="h3">
           {title}
         </StyledHeading>
         <DateInfo data-testid={TEST_ID.CARD.DATE_INFO} date={created} />
