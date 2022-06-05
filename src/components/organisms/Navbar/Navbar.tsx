b2d31d35d0b499580b6cfb8b854d79ec0684c4b2
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2022-06-05 21:08:34 +0200

    feat: create the responsive styles for <Navbar>

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -13,50 +13,128 @@ import { VisuallyHidden } from '~/utils';
 
 import type { VariantColorValueProp } from '~/theme/mixins';
 
+const { media } = styledMixin;
+
 const StyledHeader = styled.header<Required<VariantColorValueProp>>`
   position: fixed;
-  left: 0;
-  top: 0;
+  bottom: 0;
+  right: 0;
   background-color: ${styledMixin.variantColorValue()};
+  z-index: ${({ theme }) => theme.zIndex.navbar};
+  box-shadow: 0px -3px 3.5px rgba(0, 0, 0, 0.16);
+
+  ${media.greaterThan('sm')`
+    top: 0;
+    left: 0;
+    right: auto;
+    box-shadow: 0px 3px 3.5px rgba(0, 0, 0, 0.16);
+  `}
 `;
 
 const StyledNav = styled.nav`
-  padding: 25px 0;
-  width: 150px;
-  height: 100vh;
+  width: 100vw;
+  padding: 0.7rem 1.5rem;
+
+  ${media.greaterThan('sm')`
+    padding: 5.8rem 0 2.2rem;
+    top: 0;
+    left: 0;
+    width: 13rem;
+    height: 100vh;
+  `};
+
+  ${media.greaterThan('xl')`
+    width: 15rem;
+  `};
 `;
 
-const StyledLogoLink = styled(NavLink)`
+const StyledLogo = styled.img`
   display: block;
-  width: 67px;
-  height: 67px;
-  background-image: url(${logoIcon});
-  background-repeat: no-repeat;
-  background-position: 50% 50%;
-  background-size: 80%;
-  border: none;
+  width: 7.3rem;
+  object-fit: contain;
 `;
 
-const StyledLinksList = styled.ul`
-  margin: 0;
-  padding: 0;
-  list-style: none;
+const StyledMainLinksList = styled.ul`
+  display: grid;
+  width: 100%;
+  grid-template-columns: 3fr auto;
+
+  ${media.greaterThan(`sm`)`
+    display: flex;
+    height: 100%;
+    justify-content: space-between;
+    width: auto;
+    flex-direction: column;
+    align-items: center;
+  `}
 `;
 
-const StyledMainLinksList = styled(StyledLinksList)`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
+const StyledTypesOfNotesLinkList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+
+  width: 100%;
+
+  > li:first-of-type {
+    justify-self: start;
+  }
+
+  ${media.greaterThan('sm')`
+    display: block;
+  `}
+
+  > li {
+    margin-bottom: 1rem;
+    margin-bottom: clamp(1rem, -4.7rem + 6.3vw, 2.9rem);
+  }
+
+  @media screen and (max-height: 47.9em) {
+    > li {
+      margin-bottom: 1rem;
+    }
+  }
+
+  ${media.lessThan('sm')`
+    > li {
+      margin-bottom: 0;
+    }
+  `}
 `;
 
 const StyledLogoListItem = styled.li`
-  margin-bottom: 10vh;
+  display: none;
+
+  ${media.greaterThan('sm')`
+    display: block;
+    margin-bottom: 8rem;
+    // 80px for 1400px device width, 119px for 1800px device width
+    // https://royalfig.github.io/fluid-typography-calculator/
+    margin-bottom: clamp(8rem, -5.65rem + 9.75vw, 11.9rem);
+  `}
+`;
+
+const StyledButtonIcon = styled(ButtonIcon)`
+  ${media.lessThan('sm')`
+    border-radius: 4rem;
+    height: 5rem;
+    width: 5rem;
+    background-size: 60% 60%;
+  `}
+`;
+
+const StyledLogoutButtonIcon = styled(StyledButtonIcon)`
+  background-size: 70% 50%;
+  ${media.lessThan('sm')`
+    background-size: 80%;
+  `}
 `;
 
 const StyledLogoutListItem = styled.li`
-  margin-top: auto;
+  justify-self: end;
+
+  ${media.greaterThan('sm')`
+    margin-top: auto;
+  `}
 `;
 
 const Navbar = () => {
@@ -68,34 +146,45 @@ const Navbar = () => {
       <StyledNav>
         <StyledMainLinksList>
           <StyledLogoListItem>
-            <StyledLogoLink to="/">
+            <NavLink to="/">
+              <StyledLogo src={logoIcon} alt="FavNote. logo" aria-hidden />
               <VisuallyHidden>FavNote.</VisuallyHidden>
-            </StyledLogoLink>
+            </NavLink>
           </StyledLogoListItem>
           <li>
             <VisuallyHidden>Types of notes</VisuallyHidden>
-            <StyledLinksList>
+            <StyledTypesOfNotesLinkList>
               <li>
-                <ButtonIcon as={NavLink} to="/notes" icon={penIcon} activeClassName="active">
+                <StyledButtonIcon as={NavLink} to="/notes" icon={penIcon} activeClassName="active">
                   <VisuallyHidden>Basic notes</VisuallyHidden>
-                </ButtonIcon>
+                </StyledButtonIcon>
               </li>
               <li>
-                <ButtonIcon as={NavLink} to="/twitters" icon={twitterIcon} activeClassName="active">
+                <StyledButtonIcon
+                  as={NavLink}
+                  to="/twitters"
+                  icon={twitterIcon}
+                  activeClassName="active"
+                >
                   <VisuallyHidden>Twitter notes</VisuallyHidden>
-                </ButtonIcon>
+                </StyledButtonIcon>
               </li>
               <li>
-                <ButtonIcon as={NavLink} to="/articles" icon={bulbIcon} activeClassName="active">
+                <StyledButtonIcon
+                  as={NavLink}
+                  to="/articles"
+                  icon={bulbIcon}
+                  activeClassName="active"
+                >
                   <VisuallyHidden>Article notes</VisuallyHidden>
-                </ButtonIcon>
+                </StyledButtonIcon>
               </li>
-            </StyledLinksList>
+            </StyledTypesOfNotesLinkList>
           </li>
           <StyledLogoutListItem>
-            <ButtonIcon type="button" onClick={logout} icon={logoutIcon}>
+            <StyledLogoutButtonIcon type="button" onClick={logout} icon={logoutIcon}>
               <VisuallyHidden>Logout</VisuallyHidden>
-            </ButtonIcon>
+            </StyledLogoutButtonIcon>
           </StyledLogoutListItem>
         </StyledMainLinksList>
       </StyledNav>
