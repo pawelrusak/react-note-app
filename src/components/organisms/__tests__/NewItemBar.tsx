0545a31624854ac311a1d897520ac8bc7fb89725
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2022-06-14 19:40:48 +0200

    test: create tests for subheading of NewItemForm

--- src/components/organisms/__tests__/NewItemBar.tsx
+++ src/components/organisms/__tests__/NewItemBar.tsx
@@ -32,10 +32,16 @@ const queryByTwitterPlaceholderText = () => screen.queryByPlaceholderText(/twitt
 const queryByLinkPlaceholderText = () => screen.queryByPlaceholderText(/link/i);
 const getAllByHeadingRole = () => screen.getAllByRole('heading');
 const getByAddNoteButtonRole = () => screen.getByRole('button', { name: /add note/i });
+const queryByNoteSubheading = () => screen.queryByTestId('NewItemForm_NoteSubheading');
+const queryByTwitterSubheading = () => screen.queryByTestId('NewItemForm_TwitterSubheading');
+const queryByArticleSubheading = () => screen.queryByTestId('NewItemForm_ArticleSubheading');
 
 const TEST_NAME = {
   TWITTER_NAME_INPUT: 'twitter user input',
   ARTICLE_URL_INPUT: 'article URL input',
+  NOTE_SUBHEADING: 'should be displayed with a specific subheading for the note type',
+  TWITTER_SUBHEADING: 'should be displayed with a specific subheading for the twitter type',
+  ARTICLE_SUBHEADING: 'should be displayed with a specific subheading for the article type',
 };
 
 const newItemBuilder = build<NewItem>({
@@ -102,6 +108,9 @@ describe('<NewItemBar />', () => {
   })
     .not.toBeInTheDocument(TEST_NAME.TWITTER_NAME_INPUT, queryByTwitterPlaceholderText)
     .not.toBeInTheDocument(TEST_NAME.ARTICLE_URL_INPUT, queryByLinkPlaceholderText)
+    .toBeInTheDocument(TEST_NAME.NOTE_SUBHEADING, queryByNoteSubheading)
+    .not.toBeInTheDocument(TEST_NAME.TWITTER_SUBHEADING, queryByTwitterSubheading)
+    .not.toBeInTheDocument(TEST_NAME.ARTICLE_SUBHEADING, queryByArticleSubheading)
     .run();
 
   testComponent(() => renderNewItemBar(ROUTES_PATHS.twitters), {
@@ -109,6 +118,9 @@ describe('<NewItemBar />', () => {
   })
     .toBeInTheDocument(TEST_NAME.TWITTER_NAME_INPUT, queryByTwitterPlaceholderText)
     .not.toBeInTheDocument(TEST_NAME.ARTICLE_URL_INPUT, queryByLinkPlaceholderText)
+    .not.toBeInTheDocument(TEST_NAME.NOTE_SUBHEADING, queryByNoteSubheading)
+    .toBeInTheDocument(TEST_NAME.TWITTER_SUBHEADING, queryByTwitterSubheading)
+    .not.toBeInTheDocument(TEST_NAME.ARTICLE_SUBHEADING, queryByArticleSubheading)
     .run();
 
   testComponent(() => renderNewItemBar(ROUTES_PATHS.articles), {
@@ -116,5 +128,8 @@ describe('<NewItemBar />', () => {
   })
     .toBeInTheDocument(TEST_NAME.ARTICLE_URL_INPUT, queryByLinkPlaceholderText)
     .not.toBeInTheDocument(TEST_NAME.TWITTER_NAME_INPUT, queryByTwitterPlaceholderText)
+    .not.toBeInTheDocument(TEST_NAME.NOTE_SUBHEADING, queryByNoteSubheading)
+    .not.toBeInTheDocument(TEST_NAME.TWITTER_SUBHEADING, queryByTwitterSubheading)
+    .toBeInTheDocument(TEST_NAME.ARTICLE_SUBHEADING, queryByArticleSubheading)
     .run();
 });
